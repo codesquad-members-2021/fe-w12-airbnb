b9385425bff2733ed446cf091bd78c6fa1b8fa94
17-sss <jhsonking@naver.com>
2021-02-08 20:23:26 +0900

    Update ./routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET / */
 router.get('/', (req, res, /* next */) => {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: '숙소, 체험, 장소를 모두 한 곳에서 - 에어비앤비' });
 });
 
 module.exports = router;
