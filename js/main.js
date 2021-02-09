c3a0c30dd624bba248373533dd154652546c54a1
kowoohyuk <kowoohyuk91@gmail.com>
2021-02-09 21:38:48 +0900

    nav 토글 이벤트 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const header = document.querySelector('header');
+const navToggleButton = document.querySelector('.nav__button-toggle');
+const navList = document.querySelector('.nav__list');
 
 const init = () => {
+  navToggleButton.addEventListener('click', () => navList.classList.toggle('hidden'));
   const observer = new IntersectionObserver( 
     ([e]) => e.target.classList.toggle('isSticky', e.intersectionRatio < 1),
     {threshold: [1]}
