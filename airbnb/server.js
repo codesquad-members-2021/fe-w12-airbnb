aa2416fb756ecd270474e078d262be0bcabe2e89
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-02-09 11:24:26 +0900

    add. html header

---
+++ airbnb/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(8080, () => {
+  console.log("express App on port 8080!");
+});
+
+console.log(path.resolve());
