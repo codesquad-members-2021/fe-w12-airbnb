22e29d255224d3f429be30f1b28fe5b57fa842bc
swing-park <ongsim123@gmail.com>
2021-02-15 20:20:00 +0900

    add tabUI js

---
+++ js/tabUI.js
@@ -0,0 +1,20 @@
+const { log } = console;
+const info = document.querySelector(".infoBtn")
+const tabUI = document.querySelector("#tabUI");
+
+info.addEventListener('click', infoBtnClickHandler)
+
+function infoBtnClickHandler() {
+    if (tabUI.className === 'tabUI__hidden') {
+        tabUI.classList.remove('tabUI__hidden');
+        tabUI.classList.add('tabUI__show');
+        tabUI.focus();
+    }
+}
+
+function dropdownClose() {
+    if (tabUI.className === 'tabUI__show') {
+        tabUI.classList.add('tabUI__hidden');
+        tabUI.classList.remove('tabUI__show');
+    }
+}
