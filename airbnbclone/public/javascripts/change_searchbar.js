21d2057df6bf6ca2018831e171a20eebc523ca86
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-02-16 16:33:15 +0900

    [FEAT]Add profile Btn event
    프로필 버튼을 누르면 프로필 바가 나오는 이벤트 추가.

--- airbnbclone/public/javascripts/change_searchbar.js
+++ airbnbclone/public/javascripts/change_searchbar.js
@@ -1,24 +1,24 @@
-const $seachbarRooms = document.querySelector("#main_seachbar_rooms");
-const $seachbaractivity = document.querySelector("#main_seachbar_activity");
+const seachbarRooms = document.querySelector("#main_seachbar_rooms");
+const seachbaractivity = document.querySelector("#main_seachbar_activity");
 
 //<==== 질문 =--->//
 //SelectorAll 을 해서 Node[0], Node[1] 을 각자 불러오는 것이 좋을까요?
 //아니면 이렇게 서로 id 를 부여해서 불러오는 것이 좋을까요?
 
-const $navbar = document.querySelector(".navbar_ul");
-const lists = $navbar.getElementsByTagName("li");
+const navbar = document.querySelector(".navbar_ul");
+const lists = navbar.getElementsByTagName("li");
 
 const roomsBar = lists[0].querySelector(".navbar_selectedline");
 const activityBar = lists[1].querySelector(".navbar_line");
 const activityClickHandler = () => {
-  $seachbarRooms.classList.add("hide");
-  $seachbaractivity.classList.remove("hide");
+  seachbarRooms.classList.add("hide");
+  seachbaractivity.classList.remove("hide");
   roomsBar.classList.replace("navbar_selectedline", "navbar_line");
   activityBar.classList.replace("navbar_line", "navbar_selectedline");
 };
 const roomClickHandler = () => {
-  $seachbarRooms.classList.remove("hide");
-  $seachbaractivity.classList.add("hide");
+  seachbarRooms.classList.remove("hide");
+  seachbaractivity.classList.add("hide");
   activityBar.classList.replace("navbar_selectedline", "navbar_line");
   roomsBar.classList.replace("navbar_line", "navbar_selectedline");
 };
