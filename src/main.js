4013844cbffc5530637af0092216fcbe8295ded2
adelakim <diddnjs02@likelion.org>
2021-02-16 21:46:53 +0900

    [Update] Create nav.js
    - 검색바 상단에 위치한 탭에 대한 동작을 nav.js로 옮김
    - NavTab이라는 클래스를 새로 만들어봄
    - 객체로 만들만한 것인지는 잘 모르겠음. 좀 더 보기 좋은 코드를 만들고 싶었음.
    - 아까보다는 나은것 같은데 애매한 느낌임.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Toggle from "./toggle.js";
+import NavTab from "./nav.js";
 import { calendarEvent } from "./calendar.js";
+
+// 검색바 위에 위치한 탭
+const stay = new NavTab("stay", "experiences", "search-bar__stay", "search-bar__experiences");
+stay.eventListen();
+const experiences = new NavTab("experiences", "stay", "search-bar__experiences", "search-bar__stay");
+experiences.eventListen();
 
 // 계정 버튼
 const accountButton = document.querySelector("#account");
@@ -24,20 +31,6 @@ const isChecked = (button) => {
   return button.classList.contains("checked") ? true : false;
 };
 
-// const createCalendar = (calendarElement) => {
-//   const today = new Date();
-//   for (let i = 0; i < 2; i++) {
-//     const calendar = new Calendar([today.getFullYear(), today.getMonth() + i], calendarElement);
-//     calendar.setCalendarData(calendar.day.getFullYear(), calendar.day.getMonth() + 1);
-//   }
-// };
-
-// const calendarEvent = (item) => {
-//   item.element.innerHTML = ``;
-//   createCalendar(item.element);
-//   item.init();
-// };
-
 checkInButton.addEventListener("click", () => {
   if (calendarCheckOut.has("visible")) calendarCheckOut.rename("visible", "hidden");
   // 현재 탭 클릭 시 그림자 효과 주기 위한 checked 클래스 추가
@@ -53,7 +46,6 @@ checkOutButton.addEventListener("click", () => {
 });
 
 experienceDateButton.addEventListener("click", () => {
-  console.log(`experienceDateButton`);
   isChecked(experienceDateButton) ? experienceDateButton.classList.remove("checked") : experienceDateButton.classList.add("checked");
   calendarEvent(calendarExperienceDate);
 });
@@ -70,48 +62,22 @@ document.addEventListener("click", (e) => {
 
     if (!isContain(checkInButton, e.target) && !isContain(calendarCheckIn.element, e.target)) {
       // 체크인 버튼과 체크인 달력의 외부를 클릭했을 때
-      console.log("outside of calendarCheckIn");
+      //   console.log("outside of calendarCheckIn");
       calendarCheckIn.hide();
       checkInButton.classList.remove("checked");
     }
     if (!isContain(checkOutButton, e.target) && !isContain(calendarCheckOut.element, e.target)) {
       // 체크아웃 버튼과 체크아웃 달력의 외부를 클릭했을 때
-      console.log("outside of calendarCheckOut");
+      //   console.log("outside of calendarCheckOut");
       calendarCheckOut.hide();
       checkOutButton.classList.remove("checked");
     }
     if (!isContain(experienceDateButton, e.target) && !isContain(calendarExperienceDate.element, e.target)) {
-      console.log(`outside of calendarExperienceDate`);
+      //   console.log(`outside of calendarExperienceDate`);
       calendarExperienceDate.hide();
       experienceDateButton.classList.remove("checked");
     }
 
     accountLayer.hide();
   }
-});
-
-// 숙소, 체험 탭 테스트
-const stay = document.querySelector(".stay");
-const experiences = document.querySelector(".experiences");
-const searchBarForStay = document.querySelector(".search-bar__stay");
-const searchBarForExperiences = document.querySelector(".search-bar__experiences");
-
-stay.addEventListener("click", () => {
-  console.log("stay");
-
-  experiences.childNodes[3].classList.remove("set__underBar");
-  stay.childNodes[3].classList.add("set__underBar");
-
-  searchBarForStay.classList.remove("none");
-  searchBarForExperiences.classList.add("none");
-});
-
-experiences.addEventListener("click", () => {
-  console.log("experiences");
-
-  stay.childNodes[3].classList.remove("set__underBar");
-  experiences.childNodes[3].classList.add("set__underBar");
-
-  searchBarForExperiences.classList.remove("none");
-  searchBarForStay.classList.add("none");
 });
