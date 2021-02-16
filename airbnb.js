4ea388384214288eb8c02d187f97d059cd914869
chc8909 <chc8909@gmail.com>
2021-02-16 12:25:31 +0900

    Add : 숙소 체험 눌렀을 시 UI 변경

--- airbnb.js
+++ airbnb.js
@@ -1,7 +1,9 @@
 const dropMenu = document.querySelector(".drop-menu");
 
+dropMenu.addEventListener("click", clickHandler);
 
-const template = `<div>
+function clickHandler() {
+    const template = `<div>
                     <a>회원가입</a>
                     <a>로그인</a>
                 </div>
@@ -11,17 +13,69 @@ const template = `<div>
                     <a>도움말</a>
                 </div>`
 
-let div = document.createElement("div");
-div.innerHTML = template;
-dropMenu.appendChild(div);
+    let div = document.createElement("div");
+    div.innerHTML = template;
+    dropMenu.appendChild(div);
 
-div.classList.add("drop-content")
+    div.classList.add("drop-content");
 
-const dropContent = document.querySelector(".drop-content");
+    const dropContent = document.querySelector(".drop-content");
 
-
-dropMenu.addEventListener("click", clickHandler);
-
-function clickHandler(e) {
     dropContent.classList.toggle('drop-show')
+}
+
+// 체험 눌렀을 때
+const search = document.querySelector(".search");
+const menuList = document.querySelectorAll(".menu-list");
+const menuListBtns = document.querySelectorAll(".menu-list button");
+const exprienceBtn = menuListBtns[1];
+
+exprienceBtn.addEventListener("click", exprienceBtnHandler);
+
+function exprienceBtnHandler() {
+    let template = `
+        <div>
+            <span>위치</span><br>
+            <input type="text"placeholder="어디로 여행가세요?">
+        </div>
+        <div>
+            <span>날짜</span><br>
+            <span>원하는 날짜를 입력하세요</span>
+            <button class="search-btn">
+                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 20px; width: 20px; stroke: currentcolor; stroke-width: 4; overflow: visible;"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
+            </button>
+        </div>`
+
+    search.innerHTML = template;
+    search.classList.add("exprienceBtn-search")
+}
+
+// 숙소 눌렀을 때
+const roomsBtn = menuListBtns[0];
+roomsBtn.addEventListener("click", roomBtnHandler);
+
+function roomBtnHandler() {
+    let template = `
+    <div>
+        <span>위치</span><br>
+        <input type="text" placeholder="어디로 여행가세요?">
+    </div>
+    <div>
+        <span>체크인</span><br>
+        <span>날짜추가</span>
+    </div>
+    <div>
+        <span>체크아웃</span><br>
+        <span>날짜추가</span>
+    </div>
+    <div>
+        <span>인원</span><br>
+        <span>게스트추가</span>
+        <button class="search-btn">
+            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 20px; width: 20px; stroke: currentcolor; stroke-width: 4; overflow: visible;"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
+        </button>
+    </div>`
+
+    search.innerHTML = template;
+    search.classList.remove("exprienceBtn-search")
 }
