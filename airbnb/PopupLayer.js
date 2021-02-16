1f1d5ab364680f86c09dc67ac086bb6fe31504a8
jeonyeonkyu <jyk6915@gmail.com>
2021-02-16 15:34:41 +0900

    Fix: calender 버튼 클릭시 창 바뀔때 데이터를 유지하는 걸로보아 해당 창이 데이터를 가지고 있어야 겠다고 생각하여 수정

--- airbnb/PopupLayer.js
+++ airbnb/PopupLayer.js
@@ -1,8 +1,9 @@
 // const _ = { $: (selector, base = document) => base.querySelector(selector) };
 
 class PopupLayer {
-  constructor($searchInput, $nav) {
-    this.$searchInput = $searchInput;
+  constructor($roomsBox, $experienceBox, $nav) {
+    this.$roomsBox = $roomsBox;
+    this.$experienceBox = $experienceBox;
     this.$nav = $nav;
   }
 
@@ -11,59 +12,29 @@ class PopupLayer {
     this.$nav.addEventListener('click', this.changeTemplateHandler.bind(this));
   }
 
-  roomsTemplate() {
-    return `<div><a href="#">
-               <div>위치</div> <input type="text" placeholder="어디로 여행가세요?">
-            </a></div>
-            <div><a href="#">
-              <div>체크인</div> <input type="datetime" placeholder="날짜 추가">
-            </a></div>
-            <div><a href="#">
-              <div>체크아웃</div> <input type="datetime" placeholder="날짜 추가">
-            </a></div>
-            <div><a href="#">
-              <div>인원</div> <input type="number" placeholder="게스트 추가">
-            </a></div>
-           <div>
-             <button class="search_button" style="text-align: center;">
-               <img src="./images/magnifyingGlass.svg" class="magnifying_glass">
-             </button>
-           </div>`;
-  }
-
-  experienceTemplate() {
-    return `<div style="width:52%"><a href="#">
-                <div style="margin:0 39px">위치</div> <input type="text" placeholder="어디로 여행가세요?" style="width:80.5%">
-            </a></div>
-            <div style="width:52%"><a href="#">
-              <div style="margin:0 39px">날짜</div> <input type="datetime" placeholder="날짜 추가" style="width:80.5%">
-            </a></div>
-            <div>
-              <button class="search_button" style="text-align: center;">
-                <img src="./images/magnifyingGlass.svg" class="magnifying_glass">
-              </button>
-            </div>`;
-  }
-
   changeRadioButtonHandler({ target }) {
     target.previousElementSibling.checked = true;
   }
 
   changeTemplateHandler({ target }) {
+    console.log(target.className)
     switch (target.className) {
       case 'rooms':
-        this.$searchInput.innerHTML = this.roomsTemplate();
+        this.$roomsBox.classList.remove('display_none');
+        this.$experienceBox.classList.add('display_none');
         break;
       case 'experience':
-        this.$searchInput.innerHTML = this.experienceTemplate();
+        this.$roomsBox.classList.add('display_none');
+        this.$experienceBox.classList.remove('display_none');
         break;
     }
   }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  const $searchInput = document.querySelector('.search_input');
+  const $roomsBox = document.querySelector('.rooms_box');
+  const $experienceBox = document.querySelector('.experience_box');
   const $nav = document.querySelector('.nav');
-  const popupLayer = new PopupLayer($searchInput, $nav);
+  const popupLayer = new PopupLayer($roomsBox, $experienceBox, $nav);
   popupLayer.initEvent();
 })
