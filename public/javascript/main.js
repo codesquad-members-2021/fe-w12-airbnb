c52b9a47d42dee4058fb92b682338a58d2beef95
SEONKYU\difke <somedaycode@gmail.com>
2021-02-16 17:51:42 +0900

    체험 클릭시 form의 input들 변경 *아직 모듈화를 진행하지 않았음 체험 클릭시 위치를 제외한 나머지 input을 display none 날짜 label을 위치 label과 함께 화면에 보여준다.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,13 +1,42 @@
 import ProfileMenu from './profileMenu.js';
 
+class TopMenu {
+  constructor(targetEl) {
+    this.targetEl = targetEl;
+    this.accommodations = targetEl[0];
+    this.activity = targetEl[1];
+  }
+
+  changeFormForActivity() {
+    this.activity.addEventListener('click', this.activityFormHandler);
+  }
+
+  activityFormHandler() {
+    const detailsForRooms = document.querySelectorAll('.input-for-rooms');
+    const dateInput = document.querySelector('.input-date-hidden');
+    detailsForRooms.forEach((input) =>
+      input.classList.add('check-inout-hidden')
+    );
+    dateInput.classList.remove('input-date-hidden');
+    dateInput.classList.add('input-date');
+  }
+}
+
 const showProfileTab = () => {
   const navProfileBtn = document.querySelector('.nav-btn.profile');
   const profileMenu = new ProfileMenu(navProfileBtn);
   profileMenu.onEvents();
 };
 
+const changeForm = () => {
+  const topMenuLists = document.querySelectorAll('.top-list-words');
+  const topmenu = new TopMenu(topMenuLists);
+  topmenu.changeFormForActivity();
+};
+
 const main = () => {
   showProfileTab();
+  changeForm();
 };
 
 main();
