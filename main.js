e282b7357739f7a642158ec411bcac5f128db752
Juddroid <nobllizdc@gamil.com>
2021-02-16 21:35:21 +0900

    Header menu: click하면 고정되는 기능 추가 / 하지만 아직 한 번 클릭하면 돌아올 수 없는 강임

--- main.js
+++ main.js
@@ -5,46 +5,54 @@
   const $airbnbBtn = document.querySelector('#airbnb--button');
   const $activityBtn = document.querySelector('#activity--button');
   const $onlineActBtn = document.querySelector('#online--activity--button');
-  const $longBar = document.querySelector('.low-bar-long');
 
   console.log($headerButtonGrounp);
 
-  $activityBtn.addEventListener('click', () => changeBar($activityBtn));
+  $airbnbBtn.addEventListener('click', () => longBar($airbnbBtn));
+  $airbnbBtn.addEventListener('mouseenter', () => shortBar($airbnbBtn));
+  $airbnbBtn.addEventListener('mouseleave', () => transparentBar($airbnbBtn));
+
+  $activityBtn.addEventListener('click', () => longBar($activityBtn));
   $activityBtn.addEventListener('mouseenter', () => shortBar($activityBtn));
-  $activityBtn.addEventListener('mouseleave', () => lowBar($activityBtn));
+  $activityBtn.addEventListener('mouseleave', () => transparentBar($activityBtn));
+
   $onlineActBtn.addEventListener('mouseenter', () => shortBar($onlineActBtn));
-  $onlineActBtn.addEventListener('mouseleave', () => lowBar($onlineActBtn));
+  $onlineActBtn.addEventListener('mouseleave', () => transparentBar($onlineActBtn));
 
   cursorInit();
 
   // 양이 엄청 날 것 같다...
   // 이걸 다 클래스로 만들어야겠지?...
+
   function cursorInit() {
     if ($headerButtonGrounp.querySelector('.low-bar-long')) {
       document.querySelector('.button').classList.replace('button', 'button--default');
     }
   }
 
-  function changeBar(btn) {
-    if (btn.querySelector('.lower--box').classList.contains('low-bar-long')) return;
-    btn.querySelector('.lower--box').classList.replace('low-bar-short', 'low-bar-long');
+  function isLongBar(btn) {
+    return btn.querySelector('.lower--box').classList.contains('low-bar-long');
+  }
+
+  function longBar(btn) {
+    cursorInit();
+    if (isLongBar($headerButtonGrounp)) {
+      $headerButtonGrounp.querySelector('.lower--box').classList.replace('low-bar-long', 'low-bar-transparent');
+    }
     btn.querySelector('.lower--box').classList.replace('low-bar-short', 'low-bar-long');
   }
 
   function shortBar(btn) {
-    // cursor change.. 분리할 수 있나..
-    // 그런데 이렇게 하면 mouseenter가 됐을 때에서야 커서가 바뀐다
-    if (btn.querySelector('.lower--box').classList.contains('low-bar-long')) {
-      btn.classList.remove('button');
-      console.log(btn.classList);
-    }
+    if (isLongBar(btn)) return;
     btn.querySelector('.lower--box').classList.replace('low-bar-transparent', 'low-bar-short');
     btn.querySelector('.menu').className = 'menu__hover';
   }
 
-  function lowBar(btn) {
-    btn.querySelector('.menu__hover').className = 'menu';
-    if (btn.querySelector('.lower--box').classList.contains('low-bar-long')) return;
+  function transparentBar(btn) {
+    if (btn.querySelector('.menu__hover')) {
+      btn.querySelector('.menu__hover').className = 'menu';
+    }
+    if (isLongBar(btn)) return;
     btn.querySelector('.lower--box').classList.replace('low-bar-short', 'low-bar-transparent');
   }
 })(window, document);
