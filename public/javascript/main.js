77c9bf295244e4f073dbe10ad881c984f7d84bb3
SEONKYU\difke <somedaycode@gmail.com>
2021-02-16 16:38:07 +0900

    profile Tab 클릭 이벤트 추가 tabUI.js로 부터 클래스를 가져온다 main 함수 실행.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import TabUI from './tabUI.js';
+
+const showProfileTab = () => {
+  const navProfileBtn = document.querySelector('.nav-btn.profile');
+  const tabUI = new TabUI(navProfileBtn);
+  tabUI.onEvents();
+};
+
+const main = () => {
+  showProfileTab();
+};
+
+main();
