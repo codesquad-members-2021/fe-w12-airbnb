b75a5ba99ba4cf05632db250634353030424a77d
Juddroid <nobllizdc@gamil.com>
2021-02-16 18:12:38 +0900

    Header menu: 메뉴 글씨 투명도 조절기능 추가

--- main.js
+++ main.js
@@ -13,17 +13,22 @@
   $onlineActBtn.addEventListener('mouseenter', () => shortBar($onlineActBtn));
   $onlineActBtn.addEventListener('mouseleave', () => lowBar($onlineActBtn));
 
+  // 양이 엄청 날 것 같다...
+  // 이걸 다 클래스로 만들어야겠지?...
   function shortBar(btn) {
+    // cursor change.. 분리할 수 있나..
     if ($longBar) {
       document.querySelector('li').style.cursor = 'default';
     }
     if (btn.querySelector('.lower--box').classList[1] === 'low-bar-short') return;
     btn.querySelector('.low-bar').style.width = '4px';
     btn.querySelector('.low-bar').style.background = '#fff';
+    btn.querySelector('.menu').style.color = 'rgba(255, 255, 255, 0.8)';
   }
 
   function lowBar(btn) {
     btn.querySelector('.low-bar').style.width = '0px';
     btn.querySelector('.low-bar').style.background = 'transparent';
+    btn.querySelector('.menu').style.color = 'rgba(255, 255, 255)';
   }
 })(window, document);
