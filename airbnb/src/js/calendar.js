372850c4f1bb9b18f168201109128bd767ea102a
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-02-17 17:45:11 +0900

    Feat. calendar previous, next month render

--- airbnb/src/js/calendar.js
+++ airbnb/src/js/calendar.js
@@ -11,14 +11,15 @@ class Calendar {
     const firstDay = this.getFirstDay(this.year, this.month); //요일
     const monthData = this.setMonthArr(this.year, this.month, firstDay); //월의 날짜가 담긴 2차원 배열
     const monthHTML = this.makeMonthDiv(monthData); //month html
+    console.log(monthData);
     const calendarHTML = this.getCalendarFormat() + monthHTML; //format + month html
     return calendarHTML;
   }
   getFirstDay(year, month) {
-    return new Date(year, month, 1).getDate();
+    return new Date(year, month, 1).getDay();
   }
   isLeaf(year, month) {
-    return year % 4 && month === 2;
+    return year % 4 === 0 && month === 2;
   }
   setMonthArr(year, month, day) {
     const monthArr = [];
@@ -26,12 +27,14 @@ class Calendar {
     const lastDay = this.isLeaf(year, month) ? 29 : MONTH_DAYS[month];
     for (let i = 1; i <= lastDay; i++) {
       const dayIndex = (day + i - 1) % 7;
-      if (dayIndex === 0 && weekArr.length !== 0) {
+      //인덱스가 0 && weekArr에 값이 하나라도 들어가있는 경우
+      if (dayIndex === 0 && weekArr[weekArr.length - 1]) {
         monthArr.push(weekArr);
         weekArr = new Array(7).fill(undefined);
       }
       weekArr[dayIndex] = i;
     }
+    monthArr.push(weekArr); //마지막 weekArr도 추가
     return monthArr;
   }
   makeDayDiv(day) {
@@ -81,20 +84,47 @@ export class CalendarView {
     this.onEvent();
   }
   onEvent() {
-    document.addEventListener('click', this.renderCalendar.bind(this));
+    document.addEventListener('click', this.handleClick.bind(this));
   }
-  renderCalendar(e) {
+  handleClick(e) {
     if (this.calendar.contains(e.target)) {
-      return;
+      if (this.isLeftArrow(e.target)) this.setPrevDate();
+      else if (this.isRightArrow(e.target)) this.setNextDate();
     } else if (this.queryDate.contains(e.target)) {
       this.calendar.classList.toggle(HIDDEN);
     } else {
       this.calendar.classList.add(HIDDEN);
     }
-    this.setCalendar();
+
+    this.renderCalendar();
   }
-  setCalendar() {
+
+  renderCalendar() {
     const calendarHTML = this.calendarModel.getCalendar();
     this.calendar.innerHTML = calendarHTML;
+  }
+  isLeftArrow(target) {
+    return (
+      target.classList.contains('prev') ||
+      target.classList.contains('fa-angle-left')
+    );
+  }
+  isRightArrow(target) {
+    return (
+      target.classList.contains('next') ||
+      target.classList.contains('fa-angle-right')
+    );
+  }
+  setPrevDate() {
+    if (--this.calendarModel.month <= 0) {
+      this.calendarModel.year--;
+      this.calendarModel.month = 12;
+    }
+  }
+  setNextDate() {
+    if (++this.calendarModel.month > 12) {
+      this.calendarModel.year++;
+      this.calendarModel.month = 1;
+    }
   }
 }
