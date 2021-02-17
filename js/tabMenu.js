c98f4db9221be6232d99c494a9dea5a95cfe5ba0
swing-park <ongsim123@gmail.com>
2021-02-17 19:14:02 +0900

    Add Calendar action & modify css

--- js/tabMenu.js
+++ js/tabMenu.js
@@ -7,19 +7,19 @@ rooms.addEventListener('click', roomsClickHandler);
 experiences.addEventListener('click', experiencesClickHandler);
 
 function roomsClickHandler() {
-    if (roomsSearchBox.className === 'rooms--hidden') {
-        roomsSearchBox.classList.remove('rooms--hidden');
-        roomsSearchBox.classList.add('rooms--show');
-        experiencesSearchBox.classList.remove('experiences--show');
-        experiencesSearchBox.classList.add('experiences--hidden');
+    if (roomsSearchBox.className === 'hidden') {
+        roomsSearchBox.classList.remove('hidden');
+        roomsSearchBox.classList.add('show');
+        experiencesSearchBox.classList.remove('show');
+        experiencesSearchBox.classList.add('hidden');
     }
 }
 
 function experiencesClickHandler() {
-    if (experiencesSearchBox.className === 'experiences--hidden') {
-        experiencesSearchBox.classList.remove('experiences--hidden');
-        experiencesSearchBox.classList.add('experiences--show');
-        roomsSearchBox.classList.remove('rooms--show');
-        roomsSearchBox.classList.add('rooms--hidden');
+    if (experiencesSearchBox.className === 'hidden') {
+        experiencesSearchBox.classList.remove('hidden');
+        experiencesSearchBox.classList.add('show');
+        roomsSearchBox.classList.remove('show');
+        roomsSearchBox.classList.add('hidden');
     }
 }
--- js/tabUI.js
+++ js/tabUI.js
@@ -4,16 +4,16 @@ const tabUI = document.querySelector("#tabUI");
 button.addEventListener('click', infoBtnClickHandler)
 
 function infoBtnClickHandler() {
-    if (tabUI.className === 'tabUI--hidden') {
-        tabUI.classList.remove('tabUI--hidden');
-        tabUI.classList.add('tabUI--show');
+    if (tabUI.className === 'hidden') {
+        tabUI.classList.remove('hidden');
+        tabUI.classList.add('show');
         tabUI.focus();
     }
 }
 
 function dropdownClose() {
-    if (tabUI.className === 'tabUI--show') {
-        tabUI.classList.add('tabUI--hidden');
-        tabUI.classList.remove('tabUI--show');
+    if (tabUI.className === 'show') {
+        tabUI.classList.add('hidden');
+        tabUI.classList.remove('show');
     }
 }
