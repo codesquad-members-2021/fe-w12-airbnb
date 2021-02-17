679aead834971f6cda15865a1e32ac97e3559824
junamee <skawn11114@gmail.com>
2021-02-17 12:59:00 +0900

    중복코드정리

--- play.js
+++ play.js
@@ -1,22 +1,95 @@
 console.log("linked")
-//matches를 iE9에서도 에러없이 사용하기 위한 코드.
-if (!Element.prototype.matches) {
-   Element.prototype.matches = Element.prototype.msMatchesSelector;
+
+class ClassCtrl {
+   constructor(el) {
+      this.el = el;
+   }
+   show() {
+      this.el.classList.remove("hide_show");
+   }
+   hide() {
+      this.el.classList.add("hide_show");
+   }
 }
 
-//* tabUI
 
+//* ScrollCtrl
+class Scroll extends ClassCtrl {
+   constructor() {
+      super()
+      this.targetEl = document;
+      this.header1 = new ClassCtrl(document.querySelector(".header1"));
+      this.header2 = new ClassCtrl(document.querySelector(".header2"));
+   }
+   onScrollEvents() {
+
+      this.targetEl.addEventListener('scroll', (evt) => {
+         let currentScrollValue = this.targetEl.documentElement.scrollTop;
+         let screenStatus;
+         console.log('currentScrollValue is ' + currentScrollValue);
+
+         if (currentScrollValue >= 60) {
+            evt.preventDefault();
+            screenStatus = false;
+         } else {
+            screenStatus = true;
+         }
+
+         if (screenStatus === true) {
+            this.header1.show();
+            this.header2.hide();
+         } else {
+            this.header1.hide();
+            this.header2.show();
+         }
+      })
+   }
+}
+
+const loadedWindow = new Scroll();
+loadedWindow.onScrollEvents();
+
+
+
+
+
+//* SearchBar_Ctrl
+//TODO: replaceChild로 바꿔보기.
+function getValue(evt) {
+   const searchWrapper1 = new ClassCtrl(document.querySelectorAll('.search_wrapper')[0]);
+   const searchWrapper2 = new ClassCtrl(document.querySelectorAll('.search_wrapper')[1]);
+   const targetStr = document.querySelectorAll('.menu_center label span');
+
+   let targetMenu = evt.target.innerText;
+   if (targetMenu === targetStr[0].textContent) {
+      searchWrapper1.show();
+      searchWrapper2.hide();
+   }
+   if (targetMenu === targetStr[1].textContent) {
+      searchWrapper1.hide();
+      searchWrapper2.show();
+   }
+   if (targetMenu === targetStr[2].textContent) {
+      location.href = "https://www.airbnb.co.kr/s/experiences/online";
+   }
+}
+
+const centerMenu = document.querySelectorAll(".menu_center>label>span");
+for (let i = 0; i < centerMenu.length; i++) {
+   centerMenu[i].addEventListener('click', (evt) => getValue(evt));
+}
+
+
+//* BtnCtrl
 class BtnUI {
    constructor(el) {
       this.el = el;
-
    }
 
    makeMenu() {
       const menuBox = document.createElement('div');
       menuBox.id = "menuBox";
-      menuBox.classList.add('toggleMenu')
-      menuBox.style.display = "none"
+      menuBox.classList.add('toggleMenu', 'hide_show');
       menuBox.innerHTML = `<ul>
       <li><a id="sign_up" href="#">회원 가입</a></li>
       <li><a href="#">로그인</a></li>
@@ -31,137 +104,21 @@ class BtnUI {
       document.body.addEventListener("click", (evt) => {
          let toggleMenuArea = evt.target.closest('.toggleMenu');
          let toggleBtnArea = evt.target.closest('.toggleBtn');
+         const menuBoxShowingCtrl = new ClassCtrl(menuBox);
+         const classExistence = menuBox.matches('.hide_show');
 
-         if (toggleBtnArea && menuBox.style.display === 'none') {
-            menuBox.style.display = 'block';
-         } else if (toggleBtnArea && menuBox.style.display === 'block') {
-            menuBox.style.display = 'none';
-         } else if (!toggleBtnArea && !toggleMenuArea && menuBox.style.display === "block") {
-            menuBox.style.display = 'none';
+         if (toggleBtnArea && classExistence) {
+            menuBoxShowingCtrl.show();
+         } else if (toggleBtnArea && !classExistence) {
+            menuBoxShowingCtrl.hide();
+         } else if (!toggleBtnArea && !toggleMenuArea && !classExistence) {
+            menuBoxShowingCtrl.hide();
          }
       })
    }
 }
 
-const toggleBtn1 = document.querySelector(".toggleBtn1")
-const btnCtrl = new BtnUI(toggleBtn1);
-btnCtrl.makeMenu();
-
-document.addEventListener("click", (evt) => {
-   const evtTarget = evt.target.closest(".toggleBtn1");
-})
-
-const toggleMenu = document.querySelectorAll(".toggleMenu");
-
-
-
-// class TabUI {
-//    constructor(targetEl) {
-//       this.targetEl = targetEl;
-//       this.body = document.querySelector("body");
-//    }
-
-//    onClickEvents() {
-//       this.targetEl.addEventListener("click", () => {
-//          console.log(!this.targetEl.matches(".hide_show"))
-//          if (this.targetEl.matches(".toggleBtn1") && (!this.targetEl.matches(".hide_show"))) {
-//             this.showClickHandler(toggleMenu[1]);
-//             this.body.addEventListener("mouseup", this.hideClickHandler1)
-//          }
-//          if (this.targetEl.matches(".toggleBtn1") && this.targetEl.matches(".hide_show")) {
-//             this.targetEl.addEventListener("mouseup", this.hideClickHandler1)
-//          }
-//          if (this.targetEl.matches("toggleBtn2")) {
-//             this.showClickHandler(toggleMenu[0]);
-//             this.body.addEventListener("mouseup", this.hideClickHandler2)
-//          }
-//       })
-//    }
-
-//    showClickHandler(target) {
-
-//       target.classList.remove("hide_show");
-//    }
-
-//    hideClickHandler1() {
-//       toggleMenu[1].classList.add("hide_show");
-//    }
-
-//    hideClickHandler2() {
-//       toggleMenu[0].classList.add("hide_show");
-//    }
-
-// }
-// const toggleBtn1 = new TabUI(document.querySelector(".toggleBtn1"));
-// const toggleBtn2 = new TabUI(document.querySelector(".toggleBtn2"));
-// const toggleMenu = document.querySelectorAll(".toggleMenu");
-// toggleBtn1.onClickEvents();
-// toggleBtn2.onClickEvents();
-
-
-
-//* scrollUI
-class Scroll {
-   constructor() {
-      this.targetEl = document;
-      this.header1 = document.querySelector(".header1");
-      this.header2 = document.querySelector(".header2");
-   }
-
-   onScrollEvents() {
-      this.targetEl.addEventListener('scroll', (evt) => {
-         let currentScrollValue = this.targetEl.documentElement.scrollTop;
-         let screenStatus;
-         console.log('currentScrollValue is ' + currentScrollValue);
-
-         if (currentScrollValue >= 60) {
-            evt.preventDefault();
-            screenStatus = false;
-         } else {
-            screenStatus = true;
-         }
-
-         if (screenStatus === true) {
-            this.header1.classList.remove("hide_show");
-            this.header2.classList.add("hide_show");
-         } else {
-            this.header1.classList.add("hide_show");
-            this.header2.classList.remove("hide_show");
-         }
-      })
-   }
-}
-
-
-const loadedWindow = new Scroll();
-loadedWindow.onScrollEvents();
-
-
-
-function getValue(evt) {
-   const searchWrapper = document.querySelectorAll('.search_wrapper');
-   let targetMenu = evt.target.innerText;
-   if (targetMenu === '체험') {
-      searchWrapper[0].classList.add('hide_show');
-      searchWrapper[1].classList.remove('hide_show');
-   }
-   if (targetMenu === "숙소") {
-      searchWrapper[0].classList.remove('hide_show');
-      searchWrapper[1].classList.add('hide_show');
-   }
-}
-
-const centerMenu = document.querySelectorAll(".menu_center>label>span");
-for (let i = 0; i < centerMenu.length; i++) {
-   centerMenu[i].addEventListener('click', (evt) => getValue(evt));
-}
-
-
-
-
-
-// console.log(toggleMenu[1].classList.contains("hide_show"))
-// this.targetEl.addEventListener("click", (evt) => {
-//    if (toggleMenu[1].classList.contains("hide_show") === false) {
-//       if (evt.target.parentNode.className !== "toggleList") {
-//          console.log(toggleMenu)
+const btnCtrl_1 = new BtnUI(document.querySelector(".toggleBtn1"));
+const btnCtrl_2 = new BtnUI(document.querySelector(".toggleBtn2"));
+btnCtrl_1.makeMenu();
+btnCtrl_2.makeMenu();
