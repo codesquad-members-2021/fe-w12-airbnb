2495d503224c3d0e9e2f83c8679fb852805f7481
Hannah Park <ha3158987@gmail.com>
2021-02-18 03:01:42 +0900

    Refactor: remove main class to simplify

--- fe-w12-airbnb/public/javascripts/index.js
+++ fe-w12-airbnb/public/javascripts/index.js
@@ -1,25 +1,16 @@
 /* --------------------------------------------------------------------- */
 /* -------------▶︎▶︎▶︎ main 클래스: 프로그램 시작 및 클래스 통합 ◀︎◀︎◀︎----------------*/
 /* --------------------------------------------------------------------- */
-import TabUI from "./tabUI.js";
 import PopUpLayer from "./popUpLayer.js";
+import TabUI from "./tabUI.js";
 import _ from "./utill.js";
 
-class Main {
-    constructor(){
-        this.TU = new TabUI();
-        this.PUL = new PopUpLayer();
-    }
-
-    init(){
-        this.setEvent();
-    }
-
-    setEvent(){
-        
-
-    }
+const REFERENCE = {
+    body: _.$("body"),
+    dropBox: _.$(".dropdown"),
+    hamburger: _.$(".login_set")
 }
 
-const MAIN = new Main();
-MAIN.init();
+const PUL = new PopUpLayer(REFERENCE, _);
+const TU = new TabUI(REFERENCE, _);
+
