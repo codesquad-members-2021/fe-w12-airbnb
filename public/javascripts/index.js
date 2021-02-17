fbe86faa7b2d339d5fde61af5f265a56cd4b7665
17-sss <xzxking17@gmail.com>
2021-02-17 23:58:09 +0900

    Add Calendar.js & Fix 4 files 1) Create Calendar.js to manipulate Calendar 2) Change Style and Code for Calendar

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,36 @@
-import _ from "./util.js";
-import SearchBarControl from "./index/SearchBarControl.js";
-import TabUI from "./index/TabUI.js";
+import _ from './util.js';
+import SearchBarControl from './index/SearchBarControl.js';
+import TabUI from './index/TabUI.js';
+import Calendar from './index/Calendar.js';
 
+// 상단 큰 검색바
 const inputList = _.$All('.header__main__search__options input');
-
 inputList.forEach((input) => new SearchBarControl(input).init());
 
-const tabUISetting = new TabUI(_.$('.header__main__login > .tabUI'), _.$('.header__main__login > .navBtn'));
+// 햄버거
+const tabUISetting = new TabUI(
+    _.$('.header__main__login > .tabUI'),
+    _.$('.header__main__login > .navBtn'),
+);
 tabUISetting.init();
+
+// 달력 초기 생성
+const calendarList = Array.from(
+    _.$All('.calendarWrapper > .calendarSubWrapper .calendar'),
+    (calendar) =>
+        new Calendar(calendar, calendar.querySelector('.date__dynamic')),
+);
+calendarList.forEach((calendar) => calendar.createCalendar());
+
+// 달력 (이전 / 다음) 버튼 이벤트
+_.$All('.move-month__btn').forEach((btn) => btn.addEventListener('click', () => {
+    calendarList.forEach((calendarClass) => {
+        if (_.classContains(btn, 'move-month__btn--left'))
+            calendarClass.optionMonthMinus()
+        else 
+            calendarClass.optionMonthPlus();
+        
+        calendarClass.removeAllChildNodes(); 
+        calendarClass.createCalendar();
+    });    
+}));
--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -5,6 +5,9 @@ const _ = {
     classRemove: (target, ...classNames) => target.classList.remove(...classNames),  
     classToggle: (target, className) => target.classList.toggle(className),
     classContains: (target, className) => target.classList.contains(className),
+    createElement: (tagType) => document.createElement(tagType),
+    createTextNode: (strTxt) => document.createTextNode(strTxt),
+    appendChild: (target, child) => target.appendChild(child),
 };
 
 export default _;
