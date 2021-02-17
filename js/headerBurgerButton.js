0f70e98c85c01ade783bc657ee923a27f94cc34a
Juddroid <nobllizdc@gamil.com>
2021-02-17 17:17:54 +0900

    [HEADER] Burger Button: click시 팝업기능 추가

--- js/headerBurgerButton.js
+++ js/headerBurgerButton.js
@@ -1,4 +1,24 @@
 (function (window, document) {
   'use strict';
 
+  // Burger Button
+  const $burgerButton = document.querySelector('#user__button');
+  const $redCircle = document.querySelector('.red-circle');
+  const $burgerContainer = document.querySelector('.buger__container');
+
+  $burgerButton.addEventListener('click', openBurgerBox);
+
+  window.onclick = function (e) {
+    if (e.target === $burgerContainer) {
+      $burgerContainer.classList.add('visible--hidden');
+      console.log('here');
+    }
+  };
+
+  function openBurgerBox() {
+    document.querySelector('.burger__container').classList.remove('visible--hidden');
+    $redCircle.classList.add('visible--hidden');
+  }
+
+  console.log($burgerButton);
 })(window, document);
