6d1842b2c33dc1debb7ca6baa536b0b728d5343b
Juddroid <nobllizdc@gamil.com>
2021-02-18 04:00:58 +0900

    [NAV] Calendar 틀에 문제가 있다. 자고 일어나서 그거부터 고치면 된다.

--- js/reservationBar.js
+++ js/reservationBar.js
@@ -3,9 +3,16 @@
 
   const $leftLabel = document.querySelector('.leftside__radius__box');
   const $popupLeftLabel = document.querySelector('.location__container');
+  const $checkInBtn = document.querySelector('#check-in-button');
+  const $checkOutBtn = document.querySelector('#check-out-button');
+  const $calendarBox = document.querySelector('.calendar__container');
 
   $leftLabel.addEventListener('mouseup', openLeftLabel);
   window.addEventListener('click', closeLeftLabel);
+
+  $checkInBtn.addEventListener('mouseup', openCalendar);
+  $checkOutBtn.addEventListener('mouseup', openCalendar);
+  window.addEventListener('click', closeCalendar);
 
   function openLeftLabel(e) {
     e.currentTarget.classList.replace('leftside__radius__box', 'leftside__radius__box--click');
@@ -16,5 +23,15 @@
     if (e.path.includes($leftLabel)) return;
     $popupLeftLabel.classList.add('visible--hidden');
     $leftLabel.classList.replace('leftside__radius__box--click', 'leftside__radius__box');
+  }
+
+  function openCalendar() {
+    $calendarBox.classList.remove('visible--hidden');
+  }
+
+  function closeCalendar(e) {
+    if (e.path.includes($checkInBtn)) return;
+    if (e.path.includes($checkOutBtn)) return;
+    $calendarBox.classList.add('visible--hidden');
   }
 })(window, document);
