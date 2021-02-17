9cdd830564936664a32f8dd2498f5b7a8d2245f5
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-02-17 16:56:43 +0900

    Feat. 이번달 달력 js, css

--- airbnb/src/js/calendar.js
+++ airbnb/src/js/calendar.js
@@ -1,15 +1,18 @@
-import days from './daysByMonth.js';
+import MONTH_DAYS from './daysByMonth.js';
 const HIDDEN = 'hidden';
 class Calendar {
   constructor(date = new Date()) {
     this.date = date;
+    this.year = date.getFullYear();
+    this.month = date.getMonth() + 1;
+    this.day = date.getDate();
   }
   getCalendar() {
-    const year = this.date.getFullYear();
-    const month = this.getMonth() + 1;
-    const firstDay = this.getFirstDay(year, month); //요일
-    const month = this.setMonthArr(month, firstDay); //월의 날짜가 담긴 2차원 배열
-    let calendar = this.makeMonthDiv(month);
+    const firstDay = this.getFirstDay(this.year, this.month); //요일
+    const monthData = this.setMonthArr(this.year, this.month, firstDay); //월의 날짜가 담긴 2차원 배열
+    const monthHTML = this.makeMonthDiv(monthData); //month html
+    const calendarHTML = this.getCalendarFormat() + monthHTML; //format + month html
+    return calendarHTML;
   }
   getFirstDay(year, month) {
     return new Date(year, month, 1).getDate();
@@ -19,13 +22,13 @@ class Calendar {
   }
   setMonthArr(year, month, day) {
     const monthArr = [];
-    let weekArr = [];
-    const lastDay = ifLeaf(year, month) ? 29 : days[month];
+    let weekArr = new Array(7).fill(undefined);
+    const lastDay = this.isLeaf(year, month) ? 29 : MONTH_DAYS[month];
     for (let i = 1; i <= lastDay; i++) {
       const dayIndex = (day + i - 1) % 7;
       if (dayIndex === 0 && weekArr.length !== 0) {
         monthArr.push(weekArr);
-        weekArr = [];
+        weekArr = new Array(7).fill(undefined);
       }
       weekArr[dayIndex] = i;
     }
@@ -45,24 +48,24 @@ class Calendar {
     month.forEach((week) => (weeks += this.makeWeekDiv(week)));
     return `<div class='month'>${weeks}</div>`;
   }
-  getCalendarFormat(year, month) {
-    const calendarFormat = `
+  getCalendarFormat() {
+    return `
     <div class="calendar__header">
-      <div class="calendar__btn prev"><</div>
+      <div class="calendar__btn prev"><i class="fas fa-angle-left"></i></div>
       <div class="calendar__date">
-        <div class="date__year">${year}년</div>
-        <div class="date__month">${month}월</div>
+        <div class="date__year">${this.year}년&nbsp</div>
+        <div class="date__month">${this.month}월</div>
       </div>
-      <div class="calendar__btn next">></div>
+      <div class="calendar__btn next"><i class="fas fa-angle-right"></i></div>
     </div>
     <div class="calendar__index-container">
-      <div class="calendar__index day">일</div>
-      <div class="calendar__index day">월</div>
-      <div class="calendar__index day">화</div>
-      <div class="calendar__index day">수</div>
-      <div class="calendar__index day">목</div>
-      <div class="calendar__index day">금</div>
-      <div class="calendar__index day">토</div>
+      <div class="calendar__index">일</div>
+      <div class="calendar__index">월</div>
+      <div class="calendar__index">화</div>
+      <div class="calendar__index">수</div>
+      <div class="calendar__index">목</div>
+      <div class="calendar__index">금</div>
+      <div class="calendar__index">토</div>
     </div>`;
   }
 }
@@ -72,6 +75,7 @@ export class CalendarView {
     this.queryForm = queryForm;
     this.queryDate = queryDate;
     this.calendar = calendar;
+    this.calendarModel = new Calendar();
   }
   init() {
     this.onEvent();
@@ -87,5 +91,10 @@ export class CalendarView {
     } else {
       this.calendar.classList.add(HIDDEN);
     }
+    this.setCalendar();
+  }
+  setCalendar() {
+    const calendarHTML = this.calendarModel.getCalendar();
+    this.calendar.innerHTML = calendarHTML;
   }
 }
