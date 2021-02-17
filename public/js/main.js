b11485beb8378bbf750b739fed736b63a2980a00
SEONKYU\difke <somedaycode@gmail.com>
2021-02-17 17:43:42 +0900

    캘린더 년월 제목 만드는 메소드 showCalendarTitle 추가
    현재 년도와 월을 달력 제목으로 표시해준다
    showCalender메소드 render로 이름 변경
    
    달력 바깥을 눌렀을 때 달력이 사라지게 해야하는데 그게 잘 안된다.
    ;

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,38 @@
 import { ProfileMenu } from './profileMenu.js';
 import { TopMenu } from './topMenu.js';
 
+const calendarWrapper = document.querySelector('.calendars-wrapper');
+
 class Calendar {
   constructor(target) {
     this.target = target;
+    this.days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   }
+
+  init() {}
 
   onEvents() {
     this.target.addEventListener('click', this.dateClickHandler);
+    this.render();
   }
 
   dateClickHandler() {
-    const calendarWrapper = document.querySelector('.calendars-wrapper');
     calendarWrapper?.classList.toggle('calendars-wrapper-hidden');
+  }
+
+  showCalendarTitle() {
+    const calendarTitle = document.querySelector('.calendar-title');
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth() + 1;
+    calendarTitle.textContent = `${year}년 ${month}월`;
+  }
+
+  showCalendarDays() {
+    const date = new Date();
+  }
+
+  render() {
+    this.showCalendarTitle();
   }
 }
 
