d372fba273ece7ab7d6feb54bc491076e2d11683
jeonyeonkyu <ehb6915@naver.com>
2021-02-18 01:17:17 +0900

    Feat: 캘린더 월, 일 보여주기 구현

--- airbnb/Calender.js
+++ airbnb/Calender.js
@@ -1,60 +1,113 @@
 class CalenderModel {
-  constructor() {
+  currentDate = new Date();
+  monthIndex = 0;
 
+  getToday() {
+    return this.currentDate;
+  }
+
+  getPrevMonthDate(date) {
+    return new Date(date.getFullYear(), date.getMonth() + --this.monthIndex, 1);
+  }
+
+  getNextMonthDate(date) {
+    return new Date(date.getFullYear(), date.getMonth() + ++this.monthIndex, 1);
+  }
+
+  getLastDate(date) {
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  }
+
+  getFirstDay(date) {
+    return new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+  }
+
+  getYearAndMonth(date) {
+    return `${date.getFullYear()}년 ${date.getMonth() + 1}월`;
   }
 }
 
 class CalenderView {
-  constructor($fullDate, $startDate, $endDate, $searchInputBox) {
+  constructor(model, $fullDate, $startDate, $endDate, $searchInputBox) {
+    this.model = model;
     this.$fullDate = $fullDate;
     this.$startDate = $startDate;
     this.$endDate = $endDate;
     this.$searchInputBox = $searchInputBox;
     this.makeTemplate();
+    document.querySelector('.calender_box').classList.add('display_none');
   }
 
   initEvent() {
     this.$fullDate.addEventListener('click', this.toggleButtonHandler.bind(this));
+    document.addEventListener('click', this.trClickColorHandler.bind(this));
+    document.addEventListener('click', this.leftButtonHandler.bind(this));
+    document.addEventListener('click', this.rightButtonHandler.bind(this));
   }
 
-  dateTemplate() { //작업 중..
-    return `<div class="calender_box display_none">
-              <div class="width50per border-radius32">
+  dateTemplate(model, date) {
+    const lastDate = model.getLastDate(date);
+    const firstDay = model.getFirstDay(date);
+    let dayCount = 0;
+    const firstLine = `<tr class="calender_box__table__tr">
+    ${Array.from({ length: 7 }, (_, i) => i >= firstDay ? ++dayCount : '')
+        .reduce((acc, cur) => acc + `<td>${cur}</td>`, '')}
+      </tr>`;
+
+    let nextLine = '';
+    while (dayCount < lastDate) {
+      nextLine += `<tr class="calender_box__table__tr">
+      ${Array.from({ length: 7 }, () => ++dayCount <= lastDate ? dayCount : '')
+          .reduce((acc, cur) => acc + `<td>${cur}</td>`, '')}
+        </tr>`;
+    }
+
+    return `<div class="calender_box font14">
+              <div class="width50per border-radius32 text_center calender_box__inside_box">
               </div>
-              <div class="width50per white_background border-radius32">
-                <table>
-                  <tr>
-                    <th colspan="7">2021년 2월</th>
+              <div class="width50per white_background border-radius32 text_center calender_box__inside_box">
+              <button class="calender_box--left_button"><img src="./images/leftCalenderButton.svg" alt="" style="width:10px;"></button>
+              <button class="calender_box--right_button"><img src="./images/rightCalenderButton.svg" alt="" style="width:10px;"></button>
+                <table class="calender_box__table">
+                  <tr class="font16">
+                    <th class="calender_box__inside_box--month" colspan="7">${model.getYearAndMonth(date)}</th>
                   </tr>
-                  <tr>
+                  <tr class="font12 font_gray">
                     <td>일</td><td>월</td><td>화</td><td>수</td><td>목</td><td>금</td><td>토</td>
                   </tr>
-                  <tr>
-                    <td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td>
-                  </tr>
-                  <tr>
-                    <td>8</td><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td>
-                  </tr>
-                  <tr>
-                    <td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td>
-                  </tr>
-                  <tr>
-                    <td>22</td><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td>28</td>
-                  </tr>
-                  <tr>
-                    <td>29</td><td>30</td>
-                  </tr>
+                  ${firstLine}
+                  ${nextLine}
                 </table>
               </div>  
             </div>`;
   }
 
   makeTemplate() {
-    this.$searchInputBox.insertAdjacentHTML('beforeend', this.dateTemplate());
+    this.$searchInputBox.insertAdjacentHTML('beforeend', this.dateTemplate(this.model, this.model.getToday()));
   }
 
   toggleButtonHandler() {
     document.querySelector('.calender_box').classList.toggle('display_none');
+  }
+
+  trClickColorHandler({ target }) {
+    if (target.parentElement.className === 'calender_box__table__tr' && target.innerText) {
+      target.classList.toggle('calender_box__table__td-clicked');
+    }
+  }
+
+  leftButtonHandler({ target }) {
+    if (target.closest('.calender_box--left_button')) {
+      this.$searchInputBox.insertAdjacentHTML('beforeend', this.dateTemplate(this.model, this.model.getPrevMonthDate(this.model.getToday())));
+      target.closest('.calender_box').remove();
+    }
+  }
+
+  rightButtonHandler({ target }) {
+    if (target.closest('.calender_box--right_button')) {
+      this.$searchInputBox.insertAdjacentHTML('beforeend', this.dateTemplate(this.model, this.model.getNextMonthDate(this.model.getToday())));
+      target.closest('.calender_box').remove();
+    }
   }
 }
 
@@ -65,6 +118,6 @@ window.addEventListener('DOMContentLoaded', () => {
   const $searchInputBox = document.querySelector('.search_input_box');
 
   const calendarModel = new CalenderModel();
-  const calendarView = new CalenderView($fullDate, $startDate, $endDate, $searchInputBox);
+  const calendarView = new CalenderView(calendarModel, $fullDate, $startDate, $endDate, $searchInputBox);
   calendarView.initEvent();
 })
