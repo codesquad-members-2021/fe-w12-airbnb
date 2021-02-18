4ad7d5b67f4495bcacfca0981eab27ceeee37a3e
kowoohyuk <kowoohyuk91@gmail.com>
2021-02-18 21:47:24 +0900

    main.js 기존 구현기능 리팩토링

--- js/main.js
+++ js/main.js
@@ -1,51 +1,47 @@
 const Calendar = require('./calendar');
 const header = document.querySelector('header');
-const navToggleButton = document.querySelector('.nav__button-toggle');
-const navList = document.querySelector('.nav__list');
-const modalItems = document.querySelectorAll('.input-item__modal');
-const calendarTarget = document.querySelector('#calendarTarget');
-const searchPlaceTarget = document.querySelector('.search-place');
-const startDate = document.querySelector('#startDate');
-const endDate = document.querySelector('#endDate');
-const inputItems = document.querySelectorAll('.input-item');
+const modalItems = document.querySelectorAll('.modal');
+const calendarTarget = document.querySelectorAll('.js-calendar');
+const startDate = document.querySelectorAll('.js-start-date');
+const endDate = document.querySelectorAll('.js-end-date');
+const anchorTab = document.querySelector('.anchor-tab');
+const toggleItems = document.querySelectorAll('.js-toggle');
 
 const init = () => {
-  navToggleButton.addEventListener('click', () => navList.classList.toggle('hidden'));
   const observer = new IntersectionObserver( 
     ([e]) => e.target.classList.toggle('isSticky', e.intersectionRatio < 1),
     {threshold: [1]}
   );
   observer.observe(header);
-  const calendar = new Calendar(calendarTarget, new Date(), startDate, endDate);
-  calendar.init();
-  window.addEventListener('click', ({ target }) => {
-    if(!(navList.contains(target) || navToggleButton.contains(target))) {
-      navList.classList.add('hidden');
+
+  window.addEventListener('click', ({ target }) => windowClickEvent(target));
+  anchorTab.addEventListener('click', ({ target }) => toggleCategory(target));
+  toggleItems.forEach((v, i) => v.addEventListener('click', ({ target }) => toggleItem(target)));
+  calendarTarget.forEach((v, i) => new Calendar(v, new Date(), startDate[i], endDate[i]).init());
+
+  const windowClickEvent = target => {
+    // modal 외의 영역 클릭 시 히든처리
+  }
+
+  const toggleItem = target => {
+    const name = target.dataset.target || target.parentNode.dataset.target;
+    const toggleTarget = document.querySelector(`[data-target_name="${name}"]`);
+    if(toggleTarget.classList.contains('hidden')) {
+      hideModal();
+      toggleTarget.classList.remove('hidden');
+    } else {
+      hideModal();
     }
-  }); 
-  inputItems.forEach(v => v.addEventListener('click', ({ target }) => {
-    if(target.classList.contains('js-toggle-calendar') || target.parentNode.classList.contains('js-toggle-calendar')) {
-      if(calendarTarget.classList.contains('hidden')) {
-        hiddenModal();
-        calendarTarget.classList.remove('hidden');
-      } else {
-        hiddenModal();
-      }
-      return;
-    }
-    if(target.classList.contains('js-toggle-place') || target.parentNode.classList.contains('js-toggle-place')) {
-      if(searchPlaceTarget.classList.contains('hidden')) {
-        hiddenModal();
-        searchPlaceTarget.classList.remove('hidden');
-      } else {
-        hiddenModal();
-      }
-      return;
-    }
-  }));
-  const hiddenModal = () => {
+  };
+
+  const hideModal = () => {
     modalItems.forEach(v => v.classList.add('hidden'));
   }
+
+  const toggleCategory = target => {
+    if(!target.classList.contains('category__anchor')) return;
+    console.log(target.dataset.index);
+  };
 }
 
 window.addEventListener('load', init);
