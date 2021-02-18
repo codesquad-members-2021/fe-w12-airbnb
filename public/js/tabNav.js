daef27c5cbc650f4f36c3284e3b4de31ceaee705
Jungwon Lee <leejungwon96@gmail.com>
2021-02-19 00:54:19 +0900

    add EventListener on checkIn li

--- public/js/tabNav.js
+++ public/js/tabNav.js
@@ -1,13 +1,15 @@
 class TabNav {
-  constructor(roomTab, experienceTab) {
+  constructor() {
     //
     this.radios = document.querySelectorAll("input[name='choice']");
     this.roomTab = document.querySelector(".main_reserve_container");
     this.experienceTab = document.querySelector(".main_reserve_container_2");
+    this.checkIn = document.querySelector(".checkIn");
+    this.checkOut = document.querySelector(".checkOut");
+    this.calendar = document.querySelector(".calendar_container");
     this.initTabEvent();
   }
   initTabEvent() {
-    //
     this.radios.forEach((radio) =>
       radio.addEventListener("change", (e) => {
         if (e.target.checked) {
@@ -16,8 +18,11 @@ class TabNav {
         }
       })
     );
+
+    this.checkIn.addEventListener("click", () => {
+      this.calendar.classList.toggle("flex");
+    });
   }
-  handleChange() {}
 }
 
 window.addEventListener("DOMContentLoaded", () => {
