ce3e9568bd361494dc3230d4b8ed150b768dcfe6
Juddroid <nobllizdc@gamil.com>
2021-02-19 05:52:15 +0900

    [NAV] Resevation Bar: reload 수정

--- js/main.js
+++
@@ -1,8 +0,0 @@
-(function (window, document) {
-  'use strict';
-
-  const $logoButton = document.querySelector('#logo--button');
-  $logoButton.addEventListener('click', () => re);
-
-  let re = location.reload;
-})(window, document);
