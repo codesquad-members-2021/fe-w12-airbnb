097c221e3207ecf6f2a04f75be1d9031ab3b4b0f
Jungwon Lee <leejungwon96@gmail.com>
2021-02-18 20:19:59 +0900

    refactor Calendar Class methods

--- public/js/dateTable.js
+++ public/js/dateTable.js
@@ -3,24 +3,26 @@ class Calendar {
   constructor() {
     this.today = new Date();
     this.countMonth = 0;
-    this.currentDate = new Date(
-      this.today.getFullYear(),
-      this.today.getMonth() + this.countMonth,
-      1
-    ); //현재 날짜의 month에 해당하는 1일
   }
   showCurrentCal() {
-    this.buildCalendar();
+    this.buildCalendar(".calendar", 0);
   }
   showNextCal() {
+    this.buildCalendar(".calendar2", 1);
     //
   }
 
-  buildCalendar() {
-    const calendarTable = document.querySelector(".calendar");
+  buildCalendar(calendar, num_checkNextCal) {
+    //num_checkNextCal은 다음달 캘린더이면 1 아니면 0을 받는 파라미터. 아래 함수들에서 + 해준다
+    const calendarTable = document.querySelector(calendar);
+    const currentDate = new Date(
+      this.today.getFullYear(),
+      this.today.getMonth() + this.countMonth + num_checkNextCal,
+      1
+    ); //현재 날짜의 month에 해당하는 1일
     const numOfDaysInMonth = new Date(
-      this.currentDate.getFullYear(),
-      this.currentDate.getMonth() + 1,
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
       0
     ).getDate(); //현재 날짜의 month에 해당하는 마지막 일 (ex 28, 30, 31)
 
@@ -29,7 +31,7 @@ class Calendar {
     let cell;
 
     //어디부터 1일이 시작하는지 알기 위한 for문..
-    for (let i = 0; i < this.currentDate.getDay(); i++) {
+    for (let i = 0; i < currentDate.getDay(); i++) {
       cell = row.insertCell();
       cnt += 1;
     }
@@ -38,7 +40,6 @@ class Calendar {
       cell = row.insertCell();
       cell.innerHTML = i;
       cnt++;
-
       if (cnt % 7 === 0) {
         cell.innerHTML = i;
         row = calendarTable.insertRow();
@@ -50,43 +51,5 @@ class Calendar {
 window.addEventListener("DOMContentLoaded", () => {
   let calendar = new Calendar();
   calendar.showCurrentCal();
+  calendar.showNextCal();
 });
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   const calendarTable = document.querySelector(".calendar");
-
-//   const today = new Date(); //오늘날짜
-//   const currentDate = new Date(today.getFullYear(), today.getMonth(), 1); //현재 날짜의 month에 해당하는 1일
-
-//   const numOfDaysInMonth = new Date(
-//     currentDate.getFullYear(),
-//     currentDate.getMonth() + 1,
-//     0
-//   ).getDate(); //현재 날짜의 month에 해당하는 마지막 일 (ex 28, 30, 31)
-
-//   const nextDate = new Date(
-//     currentDate.getFullYear(),
-//     currentDate.getMonth() + 2,
-//     0
-//   ).getDate(); //현재 날짜 기준 다음달에 해당하는 달의 마지막 일(ex 28, 30, 31)
-
-//   let cnt = 0;
-//   let row = calendarTable.insertRow();
-
-//   //어디부터 1일이 시작하는지 알기 위한 for문..
-//   for (let i = 0; i < currentDate.getDay(); i++) {
-//     cell = row.insertCell();
-//     cnt += 1;
-//   }
-
-//   for (let i = 1; i <= numOfDaysInMonth; i++) {
-//     cell = row.insertCell();
-//     cell.innerHTML = i;
-//     cnt++;
-
-//     if (cnt % 7 === 0) {
-//       cell.innerHTML = i;
-//       row = calendarTable.insertRow();
-//     }
-//   }
-// });
