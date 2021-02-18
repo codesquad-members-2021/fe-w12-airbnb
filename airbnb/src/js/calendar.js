4c75efdef998438cc1c94c23f5232667f7aab426
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-02-18 15:58:22 +0900

    Feat. 날짜 2개 선택 로직

--- airbnb/src/js/calendar.js
+++ airbnb/src/js/calendar.js
@@ -1,6 +1,7 @@
 import { makeDiv } from './controlHTML.js';
 import MONTH_DAYS from './daysByMonth.js';
 const HIDDEN = 'hidden';
+const CHEKCED = 'checked';
 class Calendar {
   constructor(date = new Date()) {
     this.date = date;
@@ -8,6 +9,8 @@ class Calendar {
     this.month = date.getMonth() + 1;
     this.day = date.getDate();
     this.nowDate = { year: this.year, month: this.month, day: this.day };
+    this.startReserveDay;
+    this.endReserveDay;
   }
   getCalendar() {
     const firstDay = this.getFirstDay(this.year, this.month); //요일
@@ -15,6 +18,9 @@ class Calendar {
     const monthHtml = this.makeMonthHtml(monthData);
     const calendarHtml = this.getCalendarFormat() + monthHtml;
     return calendarHtml;
+  }
+  getDateStringType(day) {
+    return `${this.month}월 ${day}일`;
   }
   getFirstDay(year, month) {
     return new Date(year, month - 1, 1).getDay();
@@ -47,8 +53,11 @@ class Calendar {
     }
   }
   makeDayHtml(day) {
-    if (day && this.isReservable(day)) return makeDiv(day, 'day', 'able');
-    else if (day) return makeDiv(day, 'day', 'past', 'disable');
+    if (day && this.isReservable(day)) {
+      if (day === this.startReserve || day === this.endReserve)
+        return makeDiv(day, 'day', 'able', 'checked');
+      else return makeDiv(day, 'day', 'able');
+    } else if (day) return makeDiv(day, 'day', 'past', 'disable');
     else return makeDiv('', 'day', 'disable');
   }
   makeWeekHtml(week) {
@@ -90,6 +99,8 @@ export class CalendarView {
     this.queryDate = queryDate;
     this.calendar = calendar;
     this.calendarModel = new Calendar();
+    this.startReserveDay;
+    this.endReserveDay;
   }
   init() {
     this.onEvent();
@@ -97,11 +108,10 @@ export class CalendarView {
   onEvent() {
     document.addEventListener('click', this.handleClick.bind(this));
   }
-  handleClick(e) {
-    if (this.calendar.contains(e.target)) {
-      if (this.isLeftArrow(e.target)) this.setPrevDate();
-      else if (this.isRightArrow(e.target)) this.setNextDate();
-    } else if (this.queryDate.contains(e.target)) {
+  handleClick({ target }) {
+    if (this.calendar.contains(target)) {
+      this.handleCalendarClick(target);
+    } else if (this.queryDate.contains(target)) {
       this.calendar.classList.toggle(HIDDEN);
     } else {
       this.calendar.classList.add(HIDDEN);
@@ -109,22 +119,23 @@ export class CalendarView {
 
     this.renderCalendar();
   }
-
+  handleCalendarClick(target) {
+    if (this.isLeftArrow(target)) this.setPrevDate();
+    else if (this.isRightArrow(target)) this.setNextDate();
+    else if (this.isDay(target)) {
+      this.setReserveDate(target);
+      console.log(target.classList);
+    }
+  }
   renderCalendar() {
     const calendarHTML = this.calendarModel.getCalendar();
     this.calendar.innerHTML = calendarHTML;
   }
-  isLeftArrow(target) {
-    return (
-      target.classList.contains('prev') ||
-      target.classList.contains('fa-angle-left')
-    );
+  isLeftArrow({ classList } = target) {
+    return classList.contains('prev') || classList.contains('fa-angle-left');
   }
-  isRightArrow(target) {
-    return (
-      target.classList.contains('next') ||
-      target.classList.contains('fa-angle-right')
-    );
+  isRightArrow({ classList } = target) {
+    return classList.contains('next') || classList.contains('fa-angle-right');
   }
   setPrevDate() {
     if (--this.calendarModel.month <= 0) {
@@ -137,5 +148,32 @@ export class CalendarView {
       this.calendarModel.year++;
       this.calendarModel.month = 1;
     }
+  }
+  isDay({ classList } = target) {
+    return classList.contains('day') && classList.contains('able');
+  }
+  setReserveDate({ innerText: day, classList } = target) {
+    day = parseInt(day);
+    const { startReserve, endReserve } = this.calendarModel;
+    if (!startReserve) this.setStartReserve(day);
+    else if (!endReserve) this.setEndReserve(day);
+    else {
+      this.setStartReserve(day);
+      if (day <= startReserve || day >= endReserve) {
+        this.clearEndReserve();
+      }
+    }
+  }
+  setStartReserve(day) {
+    this.calendarModel.startReserve = day;
+    this.startReserve = this.calendarModel.getDateStringType(day);
+  }
+  setEndReserve(day) {
+    this.calendarModel.endReserve = day;
+    this.endReserve = this.calendarModel.getDateStringType(day);
+  }
+  clearEndReserve() {
+    this.calendarModel.endReserve = 0;
+    this.endReserve = '';
   }
 }
