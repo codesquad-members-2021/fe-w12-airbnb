0d8c84baccd9299120c5ac3c6f9b38a869aef05a
Jungwon Lee <leejungwon96@gmail.com>
2021-02-18 17:32:40 +0900

    Test displaying Feb calendar on html

--- public/js/dateTable.js
+++ public/js/dateTable.js
@@ -6,21 +6,41 @@ class Calandar {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  const calendarDiv = document.querySelector(".calendar");
-  const today = new Date();
-  const currentDate = new Date(today.getFullYear(), today.getMonth(), 1);
+  const NUMBER_OF_DAYS_IN_WEEK = 7;
+  const calendarHTML = "";
+  const calendarTable = document.querySelector(".calendar");
+  const today = new Date(); //오늘날짜
+  const currentDate = new Date(today.getFullYear(), today.getMonth(), 1); //현재 날짜의 month에 해당하는 1일
 
   const thisDate = new Date(
     currentDate.getFullYear(),
     currentDate.getMonth() + 1,
     0
-  ).getDate();
+  ).getDate(); //현재 날짜의 month에 해당하는 마지막 일 (ex 28, 30, 31)
 
   const nextDate = new Date(
     currentDate.getFullYear(),
     currentDate.getMonth() + 2,
     0
-  ).getDate();
-  const currentCalendar = Array.from({ length: thisDate }, (_, i) => i + 1);
-  const nextCalendar = Array.from({ length: nextDate }, (_, i) => i + 1);
+  ).getDate(); //현재 날짜 기준 다음달에 해당하는 달의 마지막 일(ex 28, 30, 31)
+
+  let cnt = 0;
+  let row = calendarTable.insertRow();
+
+  //어디부터 1일이 시작하는지 알기 위한 for문..
+  for (let i = 0; i < currentDate.getDay(); i++) {
+    cell = row.insertCell();
+    cnt += 1;
+  }
+
+  for (let i = 1; i <= thisDate; i++) {
+    cell = row.insertCell();
+    cell.innerHTML = i;
+    cnt++;
+
+    if (cnt % 7 === 0) {
+      cell.innerHTML = i;
+      row = calendarTable.insertRow();
+    }
+  }
 });
