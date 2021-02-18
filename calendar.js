6af977e87c789d0a0c68fc6d024ed4eaa8a79441
reminderclock <reminderclock@naver.com>
2021-02-18 15:14:58 +0900

    feat: 달력 넘기는 이벤트 구현

--- calendar.js
+++ calendar.js
@@ -1,19 +1,29 @@
+const date = new Date();
+let thisYear = date.getFullYear();
+// 현재 월
+let thisMonth = date.getMonth()+1;;
 function printCalender(y,m) {
     // 날짜 객체
-    let date = new Date();
     // 현재 연도
-    let thisYear = date.getFullYear();
-    // 현재 월
-    let thisMonth = date.getMonth();
     let today = date.getDate();
+    console.log(y);
+    console.log(m);
     // console.log(thisYear, thisMonth, today?);
+    if(y!== undefined || m !== undefined) {
+        thisYear = y;
+        thisMonth = m-1;
+    }
     displayYearMonth(thisYear, thisMonth+1);
-    return (y=== undefined || m === undefined) ? createDate(thisYear, thisMonth, today): createDate(y,m,today); 
+    return createDate(thisYear, thisMonth); 
 }
-
+let cnt =1;
 function displayYearMonth(y, m) {
-    const calendarYearMonth = document.querySelector('.calendar__year-month');
+    console.log(y,m);
+    const calendarYearMonth = document.querySelector(`.calendar__year-month${cnt}`);
+    // const calendarYearMonth2 = document.querySelector('.calendar__year-month2');
     calendarYearMonth.innerHTML = `${y}년 ${m}월`;
+    // calendarYearMonth2.innerHTML = `${y}년 ${m+1}월`;
+    // let calendarYearMonth = `<div class="calendar__year-month">${y}년 ${m}월</div>`
 }
 
 function createDate(y, m, d){
@@ -29,7 +39,7 @@ function createDate(y, m, d){
         lastDateArr[1] = 29;
     }
     let rowCnt = Math.ceil((theDay+lastDate)/7);
-    const calendarDay = document.querySelector('.calendar__day');
+    const calendarDay = document.querySelector(`.calendar__day${cnt}`);
     let calendar = "<tr>";
     for(let i=0; i<weekDay.length; i++) {
         calendar += `<th>${weekDay[i]}</th>`;
@@ -49,11 +59,22 @@ function createDate(y, m, d){
     }
     calendar+= "<tr>";
 }
+
     calendarDay.innerHTML = calendar;
+    if(cnt %2 === 1) {
+        cnt++;
+        return printCalender(thisYear, thisMonth+2);
+        // return addCnt();
+    }
 }
-
-printCalender();
-
+let monthCnt =-1;
+function addCnt() {
+    monthCnt++;
+    // const date = new Date();
+    printCalender(thisYear, thisMonth+monthCnt);
+}
+// addCnt();
+// printCalender(thisYear, thisMonth);
 const subCalendarBtn = document.querySelector('.search-sub__calendarBtn');
 
 const subCalendar = document.querySelector('.calendar');
@@ -62,8 +83,14 @@ subCalendarBtn.addEventListener('click', () => {
     subCalendar.classList.toggle('active');
 });
 
-if(subCalendarBtn.className === 'calendar active') {
-    document.addEventListener('click', () => {
-        subCalendar.classList.toggle('active');
-    })
-}
+// if(subCalendarBtn.className === 'calendar active') {
+//     document.addEventListener('click', () => {
+//         subCalendar.classList.toggle('active');
+//     })
+// }
+
+const arrowRight = document.querySelector('.next-month');
+arrowRight.addEventListener('click', ()=> {
+    cnt = 1;
+    addCnt();
+})
