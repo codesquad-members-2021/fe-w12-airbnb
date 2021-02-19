de3c7a9f9cb96fb2c00ddd0a2349ab48ab36e3d9
Juddroid <nobllizdc@gamil.com>
2021-02-19 16:25:24 +0900

    [NAV] Calendar: 눈물의 첫 templating / 엄청 좋은거 같다.. 난 큐브 만들 때 뭘했던걸까...

--- js/calendar.js
+++ js/calendar.js
@@ -5,16 +5,30 @@
   let month = DATE.getMonth() + 1;
   let fullYear = DATE.getFullYear();
 
+  const $calendarWrapper = document.querySelector('.calendar__wrapper');
+
   const WEEKDAY = { 0: '일', 1: '월', 2: '화', 3: '수', 4: '목', 5: '금', 6: '토' };
 
-  const $monthlyTitle = document.querySelector('.monthly--title');
-
   // 돔 생성 (그리기)
+  class CalendarBox {
+    constructor() {}
+
+    drawTitle() {}
+    drawWeek() {}
+    drawDay() {}
+  }
 
   // title -->
-  // .upper > title
-  $monthlyTitle.innerHTML = `${fullYear}년 ${month}월`;
-  console.log($monthlyTitle);
+  // box > .upper > title
+
+  const calendarBox = `
+    <div class="calendar--upper">
+      <div class="monthly--title">
+        ${fullYear}년 ${month}월
+      </div>
+    </div>`;
+
+  $calendarWrapper.innerHTML = calendarBox;
 
   // week -->
   // .weekdays > ul > li * 7
@@ -22,8 +36,11 @@
   // day -->
   // table > tbody > tr > td * 7 > div * 7
 
-  // 데이터 생성 (분리)
+  // 데이터
+  // 생성(분리);
   // 데이터 넣기
+
+  // css 추가
 
   console.log(`DATE: ${DATE}`);
   console.log(`day: ${WEEKDAY[day]}`);
