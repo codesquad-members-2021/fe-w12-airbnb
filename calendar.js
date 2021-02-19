0e6a52b9c3bc206cbcd017135a1268ae06aed97b
junamee <skawn11114@gmail.com>
2021-02-20 01:56:16 +0900

    체크인>체크아웃:solved, 새로운 문제가 버블버블 ^ ^ .

--- calendar.js
+++ calendar.js
@@ -221,19 +221,17 @@ export class CalendarMaker {
       let checkOut = document.createElement("div");
       let clickedDiv = document.querySelectorAll('.clicked');
 
-      //체크인
+      //*체크인>체크아웃
       if (this.judgement >= 2 && (Number(el.innerText) < Number(clickedDiv[1].innerText))) {
-         console.log('2번째부터 클릭')
          let eraseClicked = Array.from(clickedDiv);
-
          eraseClicked[1].classList.remove('clicked');
-         console.log(eraseClicked)
       }
 
-      //체크인 클릭시
-      if (this.judgement % 2 !== 0) {
+      //*체크인<체크아웃
+      //체크인 선택
+      else if (this.judgement % 2 !== 0) {
          //첫번째 클릭이 아닌 경우(->기존의 클릭 삭제)
-         if (this.judgement > 1) {
+         if (this.judgement > 1 && (Number(el.innerText) > Number(clickedDiv[1].innerText))) {
             let eraseClicked = Array.from(clickedDiv);
             eraseClicked.pop();
             eraseClicked.forEach(el => {
@@ -244,14 +242,9 @@ export class CalendarMaker {
          for (let i = 0; i < paste1.length; i++) {
             checkI_O_Bar.replaceChild(checkIn, paste1[i])
          }
-         //체크아웃 클릭시
-      } else {
-         // if (Number(el.innerText) < Number(clickedDiv[1].innerText)) {
-
-         //    let eraseClicked = Array.from(clickedDiv);
-         //    console.log(eraseClicked)
-         //    eraseClicked[1].classList.remove('clicked');
-         // } else {
+      }
+      //체크아웃 클릭시
+      else if (this.judgement % 2 === 0 && (Number(el.innerText) > Number(clickedDiv[1].innerText))) {
          checkOut.innerText = `${this.current_month}월 ${el.innerText}일`;
          checkI_O_Bar.appendChild(checkOut);
       }
