574991b2465f1ff28492fded4e09f2581a537d0b
Hannah Park <ha3158987@gmail.com>
2021-02-19 16:25:35 +0900

    Feat: enable changing search box on tab click

--- fe-w12-airbnb/public/javascripts/tabUI.js
+++ fe-w12-airbnb/public/javascripts/tabUI.js
@@ -3,7 +3,7 @@
 /* --------------------------------------------------------------------- */
 
 /*
-- [v] 상단 메뉴 "숙소", "체험" 탭에 마우스 hover underline 효과를 넣는다.
+- [v] 상단 메뉴 "숙소", "체험" 탭에 마우스 hover underline 효과를 넣는다. (수정필요)
 - [v] 서치바 돋보기 버튼에 마우스 hover 색상변화 효과를 넣는다.
 - [ ] 서치바 내부에 마우스 hover 그림자 효과를 넣는다.
 - [ ] 상단 메뉴 "체험" 탭을 선택하면 아래 서치바 내용이 바뀐다.
@@ -16,6 +16,7 @@ class TabUI {
         this._ = _;
         this.tab = Array.from(ref.tab);//nodelist(3)이 들어옴. span.
         this.inputs = Array.from(ref.tabInput);
+        this.searchBox = ref.searchBox;
         this.addEvent();
     }
 
@@ -26,10 +27,48 @@ class TabUI {
     }
 
     findSelectedTab({target}){
+        const className = target.classList[0];
         target.previousElementSibling.checked = true;
+
+        if (className === "experience") {
+            this.switch2Experience();
+        } else if (className === "accommodation") {
+            this.switch2Accommodation();
+        }
     }
 
-    switchSearchBox({target}) {
+    switch2Experience() {
+        const template =
+        `<div>
+            <label>위치</label><input type="text" placeholder="어디로 여행가세요?">
+        </div>
+        <div class="sec_op">
+            <label>날짜</label><input type="text" placeholder="원하는 날짜를 입력하세요.">
+        </div>
+        <div id="search_btn">
+            <img src="images/search.svg" alt="search logo" class="search_logo">
+        </div>`;
+        this.searchBox.innerHTML = template;
+    }
+
+    switch2Accommodation() {
+        const template =
+        `<div>
+            <label>위치</label><input type="text" placeholder="어디로 여행가세요?">
+        </div>
+        <div class="sec_op">
+            <label>체크인</label><input type="text" placeholder="날짜 추가">
+        </div>
+        <div class="third_op">
+            <label>체크아웃</label><input type="text" placeholder="날짜 추가">
+        </div>
+        <div class="fourth_op">
+            <label>인원</label><input type="text" placeholder="게스트 추가">
+        </div>
+        <div id="search_btn">
+            <img src="images/search.svg" alt="search logo" class="search_logo">
+        </div>`;
+        this.searchBox.innerHTML = template;
     }
 }
 
