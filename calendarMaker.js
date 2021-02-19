8eae02930fb191fc129fcf60f1ff3717a02adde8
eve712 <62237639+eve712@users.noreply.github.com>
2021-02-19 19:50:22 +0900

    Feat: 달력의 템플릿 만드는 함수 작성

--- calendarMaker.js
+++ calendarMaker.js
@@ -8,27 +8,29 @@
 [ ] 날짜 요소 그리기 (7열 5~6행)
     → (빈 요소 ~ 이번달 1일 ~ 이번달 마지막 날짜 ~ 빈 요소)*/
 
+/*========== To Do List ===========
+    1. 매직넘버 없애기 ex) daysOfWeek = 7;
+    2. 함수 인자로 넘겨줄 수 있는 것은 constructor에서 제거
+====================================*/ 
+
 export default class CalendarMaker {
     constructor() {
-        // 함수 인자로 넘겨줄 수 있는 값은 나중에 제거하기
         this.today = new Date();
         this.year = this.today.getFullYear();
         this.month = this.today.getMonth();
-        this.firstDayName;
-        this.lastDay;
-        this.lastDayName;
+        this.firstDayName; // 1일 요일
+        this.lastDay;      // 마지막 일
+        this.lastDayName;  // 마지막 일 요일
+        this.lastDayOfWeek;// 주의 마지막 일
         this.calendarHtml = ``;
     }
     // 캘린더뷰 클래스에서 이 함수 호출
     getCalendarData(when) {
-        // 날짜지정X → 현재 년,월 달력 표시
-        if(when === 'current') {
+        if(when === 'current') { // 날짜지정X → 현재 년,월 달력 표시
             this.saveMonthInfo(this.year, this.month);
             this.makeCalendar();
-            return {year:this.year, month:this.month + 1, html:this.calendarHtml};
-        } 
-        // 날짜지정O or 버튼클릭 → 인자로 받아온 년,월 달력 표시
-        else {
+            return {'year':this.year, 'month':this.month + 1, 'html':this.calendarHtml};
+        } else { // 날짜지정O or 버튼클릭 → 인자로 받아온 년,월 달력 표시
 
         }
     }
@@ -40,16 +42,42 @@ export default class CalendarMaker {
         this.lastDay = last.getDate();     // 28
         this.lastDayName = last.getDay();  // 0(일요일)
     }
+    
     makeCalendar() {
         this.calendarHtml = this.getFirstWeek() + this.getMiddleWeek() + this.getLastWeek();
     }
+
     getFirstWeek() {
-        
+        this.lastDayOfWeek = 7 - this.firstDayName;
+        let week = `<tr>`;
+        for(let i = 0; i < this.firstDayName; i++) {
+            week += `<td></td>`
+        }
+        for(let j = 0; j < this.lastDayOfWeek; j++) {
+            week += `<td><span>${j+1}</span></td>`
+        }
+        return week + `</tr>`
     }
+
     getMiddleWeek() {
-
+        let week = `<tr>`;
+        for(let i = 0; i < 7; i++) {
+            week += `<td><span>${this.lastDayOfWeek + i + 1}</span></td>`
+        }
+        this.lastDayOfWeek += 7;
+        if(this.lastDayOfWeek + 7 >= this.lastDay) return week +`</>`
+        else this.getMiddleWeek();
     }
-    getLastWeek() {
 
+    getLastWeek() {
+        const lastDayName = this.lastDayName + 1
+        let week = `<tr>`;
+        for(let i = 0; i < lastDayName; i++) {
+            week += `<td><span>${this.lastDayOfWeek + i + 1}</span></td>`
+        }
+        for(let j = 0; j < 7 - lastDayName; j++) {
+            week += `<td></td>`
+        }
+        return week + `</tr>`
     }
 }
