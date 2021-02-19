accb0e7ba52668db8c131d733e0f0069bf3d9800
reminderclock <reminderclock@naver.com>
2021-02-19 12:59:02 +0900

    feat: 양쪽 박스 생성 다음달 이벤트 구현

--- calendar.js
+++ calendar.js
@@ -1,19 +1,21 @@
 class Calendar {
-    constructor (year, month) {
+    constructor (year, month, num) {
         this.year = year;
         this.month = month;
         this.monthArr = [1,2,3,4,5,6,7,8,9,10,11,12];
-        this.leftContainer = document.querySelector(`.calendar__day1`);
-        this.yearMonth = document.querySelector('.calendar__year-month1');
+        this.leftContainer = document.querySelector(`.calendar__day${num}`);
+        this.yearMonth = document.querySelector(`.calendar__year-month${num}`);
         this.weekDay = ['일','월','화','수','목','금','토'];
         this.calendarTableInner = '';
         this.fristDayInfo = new Date(this.year, this.month, 1);
         this.fristDay = this.fristDayInfo.getDay();
         this.lastDateArr = [31,28,31,30,31,30,31,31,30,31,30,31];
         this.lastDate = this.lastDateArr[this.month];
+        // this.calendarCalssNum = 1;
     }
     
     displayYearMonth() {
+        console.log(this.yearMonth);
         this.yearMonth.innerHTML = `
         ${this.year}년 ${this.monthArr[this.month]}월`;
         this.createWeekday();
@@ -32,6 +34,23 @@ class Calendar {
         this.leftContainer.innerHTML = `${this.calendarTableInner}`;
         // this.leftContainer.innerHTML = '';
         // this.preCalenderEvent();
+        this.isLeftCalendar()
+    }
+
+    isLeftCalendar() {
+        if(classNameCnt === 1) {
+            classNameCnt++;
+            if(thisMonth === 11) {
+                thisMonth = 0;
+                thisYear++;
+            }
+            else {
+                thisMonth++;
+            }
+            calendar1 = new Calendar(thisYear, thisMonth, classNameCnt);
+            return calendar1.displayYearMonth();
+        }
+        return;
     }
 
     isLeapYear() {
@@ -80,37 +99,40 @@ class Calendar {
     // } 
 }
 
+let classNameCnt = 1;
 const date = new Date();
 let thisYear = date.getFullYear();
 let thisMonth = date.getMonth();
-let calendar1 = new Calendar(thisYear, thisMonth); 
+let calendar1 = new Calendar(thisYear, thisMonth, classNameCnt); 
 calendar1.displayYearMonth();
 
 
 const preCalenderBtn = document.querySelector('.pre-month');
 // .search-sub__calendarBtn
 preCalenderBtn.addEventListener('click', () => {
-    if(thisMonth === 0) {
-        thisYear--;
-        thisMonth = 11;
-    }
-    else{
-        thisMonth--;
-    }
+    classNameCnt =1;
+    // if(thisMonth === 0) {
+    //     thisYear--;
+    //     thisMonth = 11;
+    // }
+    // else{
+    //     thisMonth--;
+    // }
     calendar1 = new Calendar(thisYear, thisMonth); 
     calendar1.displayYearMonth();
 })
 
 const nextCalenderBtn = document.querySelector('.next-month');
 nextCalenderBtn.addEventListener('click', () => {
-    if(thisMonth === 11) {
-        thisYear++;
-        thisMonth = 0;
-    }
-    else {
-        thisMonth++;
-    }
-    calendar1 = new Calendar(thisYear, thisMonth);
+    classNameCnt = 1;
+    // if(thisMonth === 11) {
+    //     thisYear++;
+    //     thisMonth = 0;
+    // }
+    // else {
+    //     thisMonth++;
+    // }
+    calendar1 = new Calendar(thisYear, thisMonth,classNameCnt);
     calendar1.displayYearMonth();
 })
 
