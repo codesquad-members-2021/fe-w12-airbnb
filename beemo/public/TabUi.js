e59c04f4248d209148fda29e1a4793bf6d6ef3dc
jeonyeonkyu <ehb6915@naver.com>
2021-02-19 21:34:39 +0900

    Fix: 파일 이름 수정

--- beemo/public/TabUi.js
+++ beemo/public/TabUi.js
@@ -12,6 +12,20 @@ class TabUI {
     document.addEventListener('click', this.hideTemplateHandler.bind(this));
   }
 
+  makeTemplate() {
+    this.$tabButton.insertAdjacentHTML('afterend', this.template());
+  }
+
+  hideTemplateHandler({ target }) {
+    if (!target.closest(`#${this.$tabButton.id}`) && !target.closest('.tap_ui')) {
+      _.$('.tap_ui').classList.add('display_none');
+    }
+  }
+
+  toggleButtonHandler() {
+    _.$('.tap_ui').classList.toggle('display_none');
+  }
+
   template() {
     return `<div class="tap_ui display_none">
                 <a href="#" class="tap_ui__anchor"><div>메시지</div></a>
@@ -25,20 +39,6 @@ class TabUI {
                 <a href="#" class="tap_ui__anchor"><div>도움말</div></a>
                 <a href="#" class="tap_ui__anchor"><div>로그아웃</div></a>
             </div>`;
-  }
-
-  makeTemplate() {
-    this.$tabButton.insertAdjacentHTML('afterend', this.template());
-  }
-
-  hideTemplateHandler({ target }) {
-    if (!target.closest(`#${this.$tabButton.id}`) && !target.closest('.tap_ui')) {
-      _.$('.tap_ui').classList.add('display_none');
-    }
-  }
-
-  toggleButtonHandler() {
-    _.$('.tap_ui').classList.toggle('display_none');
   }
 }
 
