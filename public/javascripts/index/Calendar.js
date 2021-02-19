e6f69d8cba10482be7aac836f9c63389178df8b7
17-sss <xzxking17@gmail.com>
2021-02-19 16:57:08 +0900

    Simplify & Fix Calendar.js 1) [Creating...] Not Select Date Check

--- public/javascripts/index/Calendar.js
+++ public/javascripts/index/Calendar.js
@@ -7,10 +7,11 @@ class Calendar {
         this.optionMonth = 0;
         
         this.calendarType = _.classContains(this.target, 'left') ? 'left' : 'right';
+        this.monthInfo = null;
         this.startDate = null;
         this.endDate = null;
         this.clickDateCnt = 0;
-
+        
         this.anotherCalendar = null;
     }    
 
@@ -29,11 +30,18 @@ class Calendar {
         calendar.endDate = null;  
     }
 
-    updateClickDateCnt() {
-        this.clickDateCnt = this._createDateBtnList(this.dynamicWrapper).length;
+    _updateClickDateCnt() {
+        this.clickDateCnt = this._createSelectDateBtnList(this.dynamicWrapper).length;
     }
-    updateAnotherClickDateCnt() {
-        this.anotherCalendar.clickDateCnt = this._createDateBtnList(this.anotherCalendar.dynamicWrapper).length;
+    _updateAnotherClickDateCnt() {
+        this.anotherCalendar.clickDateCnt = this._createSelectDateBtnList(this.anotherCalendar.dynamicWrapper).length;
+    }
+
+    /**
+     * @param {Date} monthInfo
+     */
+    _updateMonthInfo(monthInfo) {
+        this.monthInfo = monthInfo;
     }
 
     removeAllChildNodes() {
@@ -49,15 +57,15 @@ class Calendar {
     createCalendar() {        
         const today = new Date();
         const monthType = this.calendarType === 'left' ? 0 : 1;
-        const monthInfo = new Date(today.getFullYear(), today.getMonth() + (monthType + this.optionMonth));
+        this._updateMonthInfo(new Date(today.getFullYear(), today.getMonth() + (monthType + this.optionMonth)));        
 
-        const yearMonthTxtNode = _.createTextNode(`${monthInfo.getFullYear()}년 ${monthInfo.getMonth()+1}월`);
+        const yearMonthTxtNode = _.createTextNode(`${this.monthInfo.getFullYear()}년 ${this.monthInfo.getMonth()+1}월`);
         _.appendChild(this.target.querySelector('.year-month'), yearMonthTxtNode);
         
-        const startDay = monthInfo.getDay(); // 매달 시작 요일
-        const lastDate = new Date(monthInfo.getFullYear(), monthInfo.getMonth()+1, 0).getDate();    // 마지막 날짜
+        const startDay = this.monthInfo.getDay(); // 매달 시작 요일
+        const lastDate = new Date(this.monthInfo.getFullYear(), this.monthInfo.getMonth()+1, 0).getDate();    // 마지막 날짜
 
-        this._createCalenderItems(today, monthInfo, startDay, lastDate);
+        this._createCalenderItems(today, this.monthInfo, startDay, lastDate);
     }
 
     _createCalenderItems(today, monthInfo, startDay, lastDate) {
@@ -127,7 +135,7 @@ class Calendar {
             if (this.startDate && this.endDate) {
                 this._removeDataBtnStyle(this.dynamicWrapper);
                 this.initStartEndDate(this);
-                this.updateClickDateCnt();
+                this._updateClickDateCnt();
                 return;
             }
 
@@ -135,9 +143,9 @@ class Calendar {
                 this._setStartDateDataBtn(thisBtn, currClickDate);
             } else {
                 if (!this.anotherCalendar.endDate)
-                    this._setEndDateBtnOrResetStartDateBtn(thisBtn, currClickDate);                  
+                    this._setEndDateBtnOrResetStartDateBtn(thisBtn, currClickDate);
             } 
-            this.updateClickDateCnt();
+            this._updateClickDateCnt();
         } else {
             if (this.anotherCalendar.startDate && this.anotherCalendar.endDate) return;
 
@@ -149,32 +157,55 @@ class Calendar {
                 this._removeDataBtnStyle(this.anotherCalendar.dynamicWrapper); 
                 this.initStartEndDate(this.anotherCalendar);
 
-                this.updateAnotherClickDateCnt();
+                this._updateAnotherClickDateCnt();
             } else {
                 if (this.startDate && this.endDate) {
                     this._removeDataBtnStyle(this.dynamicWrapper);
                     this.initStartEndDate(this);
-                    this.updateClickDateCnt();
+                    this._updateClickDateCnt();
                     return;
                 }
 
                 if (!this.startDate)
                     this._setStartDateDataBtn(thisBtn, currClickDate);
                 else
-                    this._setEndDateBtnOrResetStartDateBtn(thisBtn, currClickDate);                
+                    this._setEndDateBtnOrResetStartDateBtn(thisBtn, currClickDate);
             }
 
-            this.updateClickDateCnt();
+            this._updateClickDateCnt();
         }
     }
 
-    // 시작 or 종료 버튼 지정되어 있는 항목 Array 형식으로 GET
-    _createDateBtnList(parentNode) {
+    // 시작 or 종료 버튼 스타일 지정되어 있는 항목 Array 형식으로 GET
+    _createSelectDateBtnList(parentNode) {
         return Array.from(_.$All('ul > li > button', parentNode)).filter(
             (btn) =>
                 _.classContains(btn, 'startDate') ||
                 _.classContains(btn, 'endDate'),
         );
+    }
+
+    // 시작 or 종료 버튼이 아닌 일반 버튼 Array 형식으로 GET
+    _createNotSelectDateBtnList(parentNode) {
+        return Array.from(_.$All('ul > li > button', parentNode)).filter(
+            (btn) =>
+                (!_.classContains(btn, 'startDate') && !_.classContains(btn, 'endDate')),
+        );
+    }
+
+
+    _createGreaterThanDateBtnList(parentNode, referenceBtn) {
+        return this._createNotSelectDateBtnList(parentNode)
+            .filter(
+                (btn) => Number(referenceBtn.innerText) < Number(btn.innerText),
+            );
+    }
+    
+    _createLowerThanDateBtnList(parentNode, referenceBtn) {
+        return this._createNotSelectDateBtnList(parentNode)
+            .filter(
+                (btn) => Number(referenceBtn.innerText) > Number(btn.innerText),
+            );
     }
 
     // 시작 or 종료 버튼 지정되어 있는 항목, 매개변수(className) 기준으로 1개만 찾음
@@ -185,7 +216,7 @@ class Calendar {
 
     // 시작 or 종료 버튼 지정되어 있는 항목, Style 제거 (css class)
     _removeDataBtnStyle(parentNode) {
-        this._createDateBtnList(parentNode).forEach((btn) => _.classRemove(btn, 'clickStatus', 'startDate', 'endDate'));
+        this._createSelectDateBtnList(parentNode).forEach((btn) => _.classRemove(btn, 'clickStatus', 'startDate', 'endDate'));
     }
 
     // 각 날짜 클릭 시, classList Add & Calendar의 startDate, endDate 설정
@@ -196,21 +227,77 @@ class Calendar {
 
     _setEndDateDataBtn(thisBtn, currClickDate) {
         _.classAdd(thisBtn, 'clickStatus', 'endDate');
-        this.endDate = currClickDate; 
+        this.endDate = currClickDate;
+        this._fillColorStartToEndDate();
     }
 
     // 다른 캘린더가 아닌 this 자체의 startDate 관련은 재설정하거나, endDate 관련 버튼을 설정함.
     _setEndDateBtnOrResetStartDateBtn(thisBtn, currClickDate) {
         if (this.startDate.valueOf() < currClickDate.valueOf()) {                        
-            this._setEndDateDataBtn(thisBtn, currClickDate)
+            this._setEndDateDataBtn(thisBtn, currClickDate);
         } else {                    
             const startDateBtn = this._findOneDataBtn(this.dynamicWrapper, 'startDate');                                        
             _.classRemove(startDateBtn, 'clickStatus', 'startDate');
             this._setStartDateDataBtn(thisBtn, currClickDate);
         }
     }
-// END ---- 만들어진 날짜 선택 버튼들 한땀 한땀 이벤트 설정 ---------------------  
 
+    // 시작 ~ 종료 날짜 확정 (새로운 객체 반환)
+    _updateCompleteStartEndData() {
+        const tmpResult = {
+            startDate: new Date(),
+            endDate: new Date()
+        };
+
+        this._updateClickDateCnt();
+        this._updateAnotherClickDateCnt();
+
+        if ( (this.clickDateCnt + this.anotherCalendar.clickDateCnt) !== 2) 
+            return tmpResult;
+        
+        if (this.startDate && this.endDate) {
+            tmpResult.startDate = this.startDate;
+            tmpResult.endDate = this.endDate;
+        } else {
+            if (this.calendarType === "right") {
+                tmpResult.startDate = this.anotherCalendar.startDate; 
+                tmpResult.endDate = this.endDate; 
+            }
+        }
+
+        return tmpResult;
+    }
+
+    // 2개의 날짜가 선택 되었을 경우 fill Color (시작 ~ 끝까지)
+    _fillColorStartToEndDate() {
+        const {startDate, endDate} = this._updateCompleteStartEndData();
+        if (!startDate || !endDate) return
+        else {
+            // startDatechk가 false 되는 경우는 단 하나. (왼쪽 캘린더에 StartDate, 오른쪽에 EndDate. 마지막 클릭이 오른쪽 캘린더라서)
+            const startDateChk = startDate.getMonth().valueOf() === this.monthInfo.getMonth().valueOf();
+
+            let startBtn, endBtn = null;
+
+            
+            if (startDateChk) {
+                
+            } else {                
+                startBtn = this._findOneDataBtn(this.anotherCalendar.dynamicWrapper, 'startDate');
+                endBtn = this._findOneDataBtn(this.dynamicWrapper, 'endDate');   
+
+                const leftNormalBtnList = this._createGreaterThanDateBtnList(this.anotherCalendar.dynamicWrapper, startBtn);
+                const rightNormalBtnList = this._createLowerThanDateBtnList(this.dynamicWrapper, endBtn);
+                                
+                console.log(leftNormalBtnList);
+                console.log(rightNormalBtnList);
+            }
+
+            console.log(startDate, endDate);
+            console.log(this.calendarType, this.monthInfo)
+        }
+    }
+
+// END ---- 만들어진 날짜 선택 버튼들 한땀 한땀 이벤트 설정 ---------------------  
 }
 
 export default Calendar;
