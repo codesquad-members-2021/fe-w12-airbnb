fca9c425b3ca0a0418b8d184037ca4d123cc3675
Hongbi <dyongdi@gmail.com>
2021-02-19 21:04:59 +0900

    Feat. 이번 달의 날짜를 2차원 배열에 달력 형태와 비슷하게 저장

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -19,3 +19,36 @@ document.addEventListener('click', (e) => {
     calendrUI.classList.add('display-none')
   }
 })
+
+// 달력 데이터
+// 일단 이번 달부터 출력해보자.
+
+// 1. 이번 달의 1일이 무슨 요일인지 알아내기
+const today = new Date();
+let firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+const whatDayOfFirst = firstDay.getDay(); // 1, 즉 월요일
+
+// 2. 이번 달의 마지막 날짜가 며칠인지 알아내기
+let lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+const lastDate = lastDay.getDate(); // 28
+
+// 3. monthArr에 숫자 채우기
+const monthArr = Array.from({ length: 6 }, () => new Array(7).fill(null));
+let num = 1;
+
+// 3-1. 1주차 채우기
+for(let i = whatDayOfFirst; i <= 6; i++) {
+  monthArr[0][i] = num;
+  num ++;
+}
+console.log(monthArr);
+
+// 3-2. 2주차 ~ 마지막주차 채우기
+for(let i = 1; i <= 5; i++) {
+  for(let j = 0; j <= 6; j++) {
+    if(num > lastDate) break;
+    monthArr[i][j] = num;
+    num++;
+  }
+}
+console.log(monthArr);
